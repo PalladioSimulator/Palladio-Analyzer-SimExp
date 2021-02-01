8ad6722b5d0961a84031d89476dd44ffd6b73040
Martina Rapp <rapp@fzi.de>
2021-02-01 17:26:43 +0100

    added failure mapping logic

--- bundles/org.palladiosimulator.simexp.pcm.binding/src/org/palladiosimulator/simexp/pcm/binding/resourceenvironment/ResourceContainerPcmModelChange.java
+++ bundles/org.palladiosimulator.simexp.pcm.binding/src/org/palladiosimulator/simexp/pcm/binding/resourceenvironment/ResourceContainerPcmModelChange.java
@@ -1,10 +1,12 @@
 package org.palladiosimulator.simexp.pcm.binding.resourceenvironment;
 
-import org.apache.log4j.Logger;
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.emf.common.util.EList;
 import org.palladiosimulator.analyzer.workflow.blackboard.PCMResourceSetPartition;
 import org.palladiosimulator.pcm.resourceenvironment.ProcessingResourceSpecification;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
+
+import tools.mdsd.probdist.api.entity.CategoricalValue;
 
 /**
  * 
@@ -13,24 +15,20 @@ import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
  * */
 public class ResourceContainerPcmModelChange extends AbstractPcmModelChange {
     
-    private static final Logger LOGGER = Logger.getLogger(ResourceContainerPcmModelChange.class);
-    
-
     public ResourceContainerPcmModelChange(String attributeName, PCMResourceSetPartition pcm) {
         super(attributeName, pcm);
     }
     
     @Override
-    void applyChange(Object object) {
-        // fixme: try to avoid this case
-        double serverNodeFailureRate = (Double) object;
-        
+    void applyChange(CategoricalValue value) { 
+        double serverNodeFailureRate = (StringUtils.equals("unavailable", value.get())) ? 1.0 : 0.0;
         EList<ResourceContainer> resourceContainers = pcm.getResourceEnvironment().getResourceContainer_ResourceEnvironment();
-        
         for (ResourceContainer resourceContainer : resourceContainers) {
             EList<ProcessingResourceSpecification> activeResourceSpecs = resourceContainer.getActiveResourceSpecifications_ResourceContainer();
             for (ProcessingResourceSpecification activeResourceSpec : activeResourceSpecs) {
                 activeResourceSpec.setMTTF(serverNodeFailureRate);
+                // FIXME: currently we assign the same failure rate both for mttf and mttr; must be modelled independently in the envDyn model later
+                activeResourceSpec.setMTTR(serverNodeFailureRate);
             }
         }
     }
