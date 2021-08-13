0841d84bd9af11af72f861b84cc18e8cfa56de7d
Martina Rapp <rapp@fzi.de>
2021-08-13 14:17:35 +0200

    refactoring: renamed interface

--- examples/org.palladiosimulator.simexp.pcm.examples.loadbalancing/src/org/palladiosimulator/simexp/pcm/examples/loadbalancing/IDynamicBayesianNetworkResourceSetManager.java
+++ examples/org.palladiosimulator.simexp.pcm.examples.loadbalancing/src/org/palladiosimulator/simexp/pcm/examples/loadbalancing/IDynamicBayesianNetworkResourceSetManager.java
@@ -2,7 +2,7 @@ package org.palladiosimulator.simexp.pcm.examples.loadbalancing;
 
 import org.eclipse.emf.ecore.resource.ResourceSet;
 
-public interface IManager {
+public interface IDynamicBayesianNetworkResourceSetManager {
 
     ResourceSet getResourceSet();
 }
