feabd286f9976ac3bc351e4d6e699084271d5ed4
Martina Rapp <rapp@fzi.de>
2021-09-08 15:12:54 +0200

    added dummy implementation for performability reward signal evalution

--- examples/org.palladiosimulator.simexp.pcm.examples.loadbalancing/src/org/palladiosimulator/simexp/pcm/examples/loadbalancing/PerformabilityRewardEvaluation.java
+++ examples/org.palladiosimulator.simexp.pcm.examples.loadbalancing/src/org/palladiosimulator/simexp/pcm/examples/loadbalancing/PerformabilityRewardEvaluation.java
@@ -1,27 +1,44 @@
 package org.palladiosimulator.simexp.pcm.examples.loadbalancing;
 
-import java.util.List;
+import java.util.Set;
 
+import org.apache.log4j.Logger;
+import org.palladiosimulator.simexp.core.entity.SimulatedMeasurement;
 import org.palladiosimulator.simexp.core.entity.SimulatedMeasurementSpecification;
-import org.palladiosimulator.simexp.core.reward.ThresholdBasedRewardEvaluator;
+import org.palladiosimulator.simexp.core.reward.RewardEvaluator;
 import org.palladiosimulator.simexp.core.state.StateQuantity;
-import org.palladiosimulator.simexp.core.util.Pair;
-import org.palladiosimulator.simexp.core.util.Threshold;
 import org.palladiosimulator.simexp.markovian.model.markovmodel.markoventity.Reward;
+import org.palladiosimulator.simexp.pcm.state.PcmMeasurementSpecification;
 
-public class PerformabilityRewardEvaluation extends ThresholdBasedRewardEvaluator {
+public class PerformabilityRewardEvaluation implements RewardEvaluator {
+    
+    private static final Logger LOGGER = Logger.getLogger(PerformabilityRewardEvaluation.class.getName());
+    
+    private PcmMeasurementSpecification responseTimeMeasurementSpec;
 
-    public PerformabilityRewardEvaluation(List<Pair<SimulatedMeasurementSpecification, Threshold>> thresholds) {
-        super(thresholds);
-        // TODO Auto-generated constructor stub
+    public PerformabilityRewardEvaluation(PcmMeasurementSpecification responseTimeMeasurementSpec) {
+        this.responseTimeMeasurementSpec = responseTimeMeasurementSpec;
     }
     
     @Override
-    public Reward<Integer> evaluate(StateQuantity quantity) {
+    public Reward<Double> evaluate(StateQuantity quantity) {
+        
+        LOGGER.info(String.format("Quantified state: %s", quantity.toString()));
+        
         /**
-         * skeleton method to implement a performablity-metric (expectation of response times)
+         * skeleton method to implement the reward signal based on response times;
+         * will be later used to calculate the total reward
          * 
          * */
+        
+        double responseTime = 0.0;
+        
+        Set<SimulatedMeasurementSpecification> tmpMeasuremntSpecs = quantity.getMeasurementSpecs();
+        Set<SimulatedMeasurement> tmpMeasurments = quantity.getMeasurements();
+        
+        for (SimulatedMeasurement simulatedMeasurement : tmpMeasurments) {
+            LOGGER.info(String.format("SimulatedMeasurement: %s", simulatedMeasurement.toString()));
+        }
         
         return null;
     }
