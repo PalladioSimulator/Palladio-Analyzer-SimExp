cc11312822036b102a0c3c7f018fcfa6d9d73b70
Martina Rapp <rapp@fzi.de>
2021-09-08 15:57:45 +0200

    implemented response-time based reward signal generation

--- examples/org.palladiosimulator.simexp.pcm.examples.loadbalancing/src/org/palladiosimulator/simexp/pcm/examples/loadbalancing/PerformabilityRewardEvaluation.java
+++ examples/org.palladiosimulator.simexp.pcm.examples.loadbalancing/src/org/palladiosimulator/simexp/pcm/examples/loadbalancing/PerformabilityRewardEvaluation.java
@@ -1,6 +1,8 @@
 package org.palladiosimulator.simexp.pcm.examples.loadbalancing;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
 import org.palladiosimulator.simexp.core.entity.SimulatedMeasurement;
@@ -8,6 +10,7 @@ import org.palladiosimulator.simexp.core.entity.SimulatedMeasurementSpecificatio
 import org.palladiosimulator.simexp.core.reward.RewardEvaluator;
 import org.palladiosimulator.simexp.core.state.StateQuantity;
 import org.palladiosimulator.simexp.markovian.model.markovmodel.markoventity.Reward;
+import org.palladiosimulator.simexp.markovian.model.markovmodel.markoventity.impl.RewardImpl;
 import org.palladiosimulator.simexp.pcm.state.PcmMeasurementSpecification;
 
 public class PerformabilityRewardEvaluation implements RewardEvaluator {
@@ -31,16 +34,44 @@ public class PerformabilityRewardEvaluation implements RewardEvaluator {
          * 
          * */
         
-        double responseTime = 0.0;
+        double measuredResponseTime = 0.0;
         
-        Set<SimulatedMeasurementSpecification> tmpMeasuremntSpecs = quantity.getMeasurementSpecs();
-        Set<SimulatedMeasurement> tmpMeasurments = quantity.getMeasurements();
+        String key = responseTimeMeasurementSpec.getName();
         
-        for (SimulatedMeasurement simulatedMeasurement : tmpMeasurments) {
+        List<SimulatedMeasurement> responseTimeMeasurement = quantity.getMeasurements()
+                .stream()
+                // lookup response time measurement: Usage Scenario: UsageScenario_Response Time
+                .filter(rt -> rt.getSpecification().equals(responseTimeMeasurementSpec.getName())) 
+                .collect(Collectors.toList());
+        
+        if (responseTimeMeasurement.size() != 1) {
+            LOGGER.error(String.format("No measured response time attached to quantified state: %s", quantity.getMeasurements().toString()));
+        } else {
+            measuredResponseTime = responseTimeMeasurement.get(0).getValue();
+        }
+        
+        
+        for (SimulatedMeasurement simulatedMeasurement : quantity.getMeasurements()) {
             LOGGER.info(String.format("SimulatedMeasurement: %s", simulatedMeasurement.toString()));
         }
         
-        return null;
+        return PerformabilityRewardSignal.create(measuredResponseTime);
+    }
+    
+    private static class PerformabilityRewardSignal extends RewardImpl<Double> {
+
+        private PerformabilityRewardSignal() {
+            super.setValue(0.0);
+        }
+
+        public static PerformabilityRewardSignal create(double responseTime) {
+            return new PerformabilityRewardSignal();
+        }
+
+        @Override
+        public String toString() {
+            return Double.toString(getValue());
+        }
     }
 
 }
