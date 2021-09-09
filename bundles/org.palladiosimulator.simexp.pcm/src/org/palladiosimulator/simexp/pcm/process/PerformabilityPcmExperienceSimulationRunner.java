9f64f1ae60e778591fd76ee034f4e62004c8edb0
Martina Rapp <rapp@fzi.de>
2021-09-09 16:16:27 +0200

    moved failurescenario model creating to initialization methdo

--- bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/process/PerformabilityPcmExperienceSimulationRunner.java
+++ bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/process/PerformabilityPcmExperienceSimulationRunner.java
@@ -6,8 +6,11 @@ import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
 import org.palladiosimulator.failuremodel.failurescenario.FailureScenarioRepository;
+import org.palladiosimulator.failuremodel.failurescenario.FailurescenarioPackage;
 import org.palladiosimulator.failuremodel.failuretype.FailureTypeRepository;
+import org.palladiosimulator.failuremodel.failuretype.FailuretypePackage;
 import org.palladiosimulator.simexp.core.process.ExperienceSimulationRunner;
+import org.palladiosimulator.simexp.core.process.Initializable;
 import org.palladiosimulator.simexp.core.state.SelfAdaptiveSystemState;
 import org.palladiosimulator.simexp.core.state.StateQuantity;
 import org.palladiosimulator.simexp.pcm.datasource.DataSource;
@@ -20,7 +23,9 @@ import org.palladiosimulator.simexp.pcm.state.failure.NodeFailureTypeCreator;
 import org.palladiosimulator.simexp.pcm.util.ExperimentProvider;
 import org.palladiosimulator.simexp.pcm.util.ExperimentRunner;
 
-public class PerformabilityPcmExperienceSimulationRunner implements ExperienceSimulationRunner {
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.ResourceSetPartition;
+
+public class PerformabilityPcmExperienceSimulationRunner implements ExperienceSimulationRunner, Initializable {
     
     private static final Logger LOGGER = Logger.getLogger(PerformabilityPcmExperienceSimulationRunner.class.getName());
 
@@ -45,23 +50,15 @@ public class PerformabilityPcmExperienceSimulationRunner implements ExperienceSi
     
     @Override
     public void simulate(SelfAdaptiveSystemState<?> sasState) {
-        LOGGER.info(String.format("Simulate performability analysis for state %s", sasState.toString()));
+        LOGGER.info(String.format("==== Run simulation for state '%s'", sasState.toString()));
 
-        try {
-            // create and inject failure models into blackboard partition
-            FailureTypeRepository failureTypeRepo = failureTypeCeator.create();
-            FailureScenarioRepository failureScenarioRepo = failureStateCreator.createRepo();
-            ExperimentProvider.get().getExperimentRunner().injectFailureScenario(failureScenarioRepo, failureTypeRepo);
-            LOGGER.info("Initialized failure scenario model.");
-        } catch (IOException e) {
-            LOGGER.error("Failed to inject failurescenario models into blackboard partition", e);
-        }
-        
         experimentProvider.getExperimentRunner().runExperiment();
         LOGGER.info("Finished experiment run.");
         
         retrieveStateQuantities(asPcmState(sasState));
         LOGGER.info("Retrieved state quantities and aggregated measurements.");
+
+        LOGGER.info(String.format("==== Done. Simulation for state '%s'", sasState.toString()));
     }
     
 
@@ -88,6 +85,30 @@ public class PerformabilityPcmExperienceSimulationRunner implements ExperienceSi
     private Set<PcmMeasurementSpecification> getMeasurementSpecs(StateQuantity stateQuantity) {
         return stateQuantity.getMeasurementSpecs().stream().filter(PcmMeasurementSpecification.class::isInstance)
                 .map(PcmMeasurementSpecification.class::cast).collect(Collectors.toSet());
+    }
+
+    @Override
+    public void initialize() {
+        // FIXME: check if failurescenario models are available in working partition
+        // lookup failure model from blackboard partition
+        
+        try {
+            // create and inject failure models into blackboard partition
+            FailureTypeRepository failureTypeRepo = failureTypeCeator.create();
+            FailureScenarioRepository failureScenarioRepo = failureStateCreator.createRepo();
+            ExperimentProvider.get().getExperimentRunner().injectFailureScenario(failureScenarioRepo, failureTypeRepo);
+            LOGGER.info("Initialized failure scenario models.");
+        } catch (IOException e) {
+            LOGGER.error("Failed to inject failurescenario models into blackboard partition", e);
+        }
+        
+        ResourceSetPartition plainPartition = ExperimentProvider.get().getExperimentRunner().getPlainWorkingPartition();
+        
+        FailureScenarioRepository failureScenarioRepo = (FailureScenarioRepository) plainPartition.getElement(FailurescenarioPackage.eINSTANCE.getFailureScenarioRepository()).get(0);;
+        FailureTypeRepository failureTypeRepo = (FailureTypeRepository) plainPartition.getElement(FailuretypePackage.eINSTANCE.getFailureTypeRepository()).get(0);
+        
+        assert failureScenarioRepo != null;
+        assert failureTypeRepo != null;
     }
     
     
