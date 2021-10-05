83aa10ef12a26cb8a696ee412d7e088ccd284327
Martina Rapp <rapp@fzi.de>
2021-10-05 11:53:56 +0200

    removed unused code

--- bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/process/PerformabilityPcmExperienceSimulationRunner.java
+++ bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/process/PerformabilityPcmExperienceSimulationRunner.java
@@ -16,11 +16,6 @@ import de.uka.ipd.sdq.workflow.mdsd.blackboard.ResourceSetPartition;
 
 public class PerformabilityPcmExperienceSimulationRunner extends PcmExperienceSimulationRunner { //implements ExperienceSimulationRunner, Initializable {
     
-//    private static final Logger LOGGER = Logger.getLogger(PerformabilityPcmExperienceSimulationRunner.class.getName());
-
-//    private final DataSource dataSource;
-//    private final ExperimentProvider experimentProvider;
-    
     private NodeFailureTypeCreator failureTypeCeator;
     private NodeFailureStateCreator failureStateCreator;
 
@@ -30,58 +25,15 @@ public class PerformabilityPcmExperienceSimulationRunner extends PcmExperienceSi
 
     public PerformabilityPcmExperienceSimulationRunner(DataSource dataSource, ExperimentProvider experimentProvider) {
         super(dataSource, experimentProvider);
-//        this.dataSource = dataSource; 
-//        this.experimentProvider = experimentProvider;
-//        
         failureTypeCeator = new NodeFailureTypeCreator();
         failureStateCreator = new NodeFailureStateCreator();
     }
 
     
-//    @Override
-//    public void simulate(SelfAdaptiveSystemState<?> sasState) {
-//        LOGGER.info(String.format("==== Run simulation for state '%s'", sasState.toString()));
-//
-//        experimentProvider.getExperimentRunner().runExperiment();
-//        LOGGER.info("Finished experiment run.");
-//        
-//        retrieveStateQuantities(asPcmState(sasState));
-//        LOGGER.info("Retrieved state quantities and aggregated measurements.");
-//
-//        LOGGER.info(String.format("==== Done. Simulation for state '%s'", sasState.toString()));
-//    }
-//    
-//
-//    private PcmSelfAdaptiveSystemState asPcmState(SelfAdaptiveSystemState<?> sasState) {
-//        if (sasState instanceof PcmSelfAdaptiveSystemState) {
-//            return (PcmSelfAdaptiveSystemState) sasState;
-//        }
-//
-//        // TODO exception handling
-//        throw new RuntimeException("");
-//    }
-//
-//
-//    private void retrieveStateQuantities(PcmSelfAdaptiveSystemState sasState) {
-//        ExperimentRunner expRunner = experimentProvider.getExperimentRunner();
-//        MeasurementSeriesResult result = dataSource.getSimulatedMeasurements(expRunner.getCurrentExperimentRuns());
-//        for (PcmMeasurementSpecification each : getMeasurementSpecs(sasState.getQuantifiedState())) {
-//            result.getMeasurementsSeries(each).ifPresent(series -> {
-//                sasState.getQuantifiedState().setMeasurement(each.computeQuantity(series), each);
-//            });
-//        }
-//    }
-//
-//    private Set<PcmMeasurementSpecification> getMeasurementSpecs(StateQuantity stateQuantity) {
-//        return stateQuantity.getMeasurementSpecs().stream().filter(PcmMeasurementSpecification.class::isInstance)
-//                .map(PcmMeasurementSpecification.class::cast).collect(Collectors.toSet());
-//    }
-
     @Override
     protected void doInitialize() {
         // FIXME: check if failurescenario models are available in working partition
         // lookup failure model from blackboard partition
-        
         try {
             // create and inject failure models into blackboard partition
             FailureTypeRepository failureTypeRepo = failureTypeCeator.create();
