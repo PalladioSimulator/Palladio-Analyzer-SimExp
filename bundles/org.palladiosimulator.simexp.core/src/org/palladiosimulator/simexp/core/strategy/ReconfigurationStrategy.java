3a895174b947339e40be77d7832442c789674735
Martina Rapp <rapp@fzi.de>
2021-10-07 14:01:06 +0200

    added logging; changed return type of monitor() to void

--- bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/strategy/ReconfigurationStrategy.java
+++ bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/strategy/ReconfigurationStrategy.java
@@ -2,6 +2,7 @@ package org.palladiosimulator.simexp.core.strategy;
 
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.palladiosimulator.simexp.core.action.Reconfiguration;
 import org.palladiosimulator.simexp.markovian.activity.Policy;
 import org.palladiosimulator.simexp.markovian.model.markovmodel.markoventity.State;
@@ -9,17 +10,34 @@ import org.palladiosimulator.simexp.markovian.model.markovmodel.markoventity.Sta
 // This class is not yet integrated in the simulated experience process. This is rather a reminder for future work. 
 // The idea is to introduce a dedicated concept for reconfiguration strategies following the MAPE-K principles.
 public abstract class ReconfigurationStrategy<T> implements Policy<Reconfiguration<T>> {
+    
+    protected static final Logger LOGGER = Logger.getLogger(ReconfigurationStrategy.class);
+    
+    private final SharedKnowledge knowledge;
+    
+    public ReconfigurationStrategy() {
+        this.knowledge = new SharedKnowledge();
+    }
+    
 
 	@Override
 	public Reconfiguration<T> select(State source, Set<Reconfiguration<T>> options) {
-		SharedKnowledge knowledge = monitor();
-		if (analyse(knowledge)) {
-			return plan(knowledge);
+	    Reconfiguration<T> reconfiguration = emptyReconfiguration();
+	    
+	    LOGGER.info("Run MAPE-K loop ...");
+		monitor(knowledge);
+		LOGGER.info("Executed 'MONITOR' step");
+		boolean isAnalyzable = analyse(knowledge);
+		LOGGER.info("Executed 'ANALYZE' step");
+		if (isAnalyzable) {
+			reconfiguration = plan(knowledge);
+			LOGGER.info("Executed 'PLANING' step");
 		}
-		return emptyReconfiguration();
+		LOGGER.info(String.format("Execute 'EXECUTION' step applying reconfiguration = %s", reconfiguration.toString()));
+		return reconfiguration;
 	}
 
-	protected abstract SharedKnowledge monitor();
+	protected abstract void monitor(SharedKnowledge knowledge);
 
 	protected abstract boolean analyse(SharedKnowledge knowledge);
 
