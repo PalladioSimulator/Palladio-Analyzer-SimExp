8a77261ed38d96f0ebe6ae10075964b3f834aecd
Martina Rapp <rapp@fzi.de>
2021-10-21 09:40:00 +0200

    added runtime exception if object could not be casted to PcmSelvAdaptiveSystemState

--- bundles/org.palladiosimulator.simexp.pcm.examples.performability/src/org/palladiosimulator/simexp/pcm/examples/performability/PerformabilityStrategy.java
+++ bundles/org.palladiosimulator.simexp.pcm.examples.performability/src/org/palladiosimulator/simexp/pcm/examples/performability/PerformabilityStrategy.java
@@ -188,7 +188,12 @@ public class PerformabilityStrategy extends ReconfigurationStrategy<QVToReconfig
              * workarournd to implement node recovery behavior until we are able to realize this as QVTO transformation
              * 
              * */
-            recoveryStrategy.execute((PcmSelfAdaptiveSystemState) sasState, knowledge);
+            if (!(sasState instanceof PcmSelfAdaptiveSystemState)) {
+                /** RestoredSelfAdaptiveSystemState */
+                throw new RuntimeException("Failed to cast object");
+            }
+            PcmSelfAdaptiveSystemState pcmSasState = (PcmSelfAdaptiveSystemState) sasState;
+            recoveryStrategy.execute(pcmSasState, knowledge);
             
             return nodeRecovery(options);
 
