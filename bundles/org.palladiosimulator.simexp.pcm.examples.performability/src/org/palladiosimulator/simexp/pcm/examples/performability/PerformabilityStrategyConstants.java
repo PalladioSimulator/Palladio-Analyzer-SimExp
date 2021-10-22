710bd14726a7ff2be4ff3f07d72db6d2ea7a7bf3
Martina Rapp <rapp@fzi.de>
2021-10-22 10:03:03 +0200

    moved commonly shared constants to separate class

---
+++ bundles/org.palladiosimulator.simexp.pcm.examples.performability/src/org/palladiosimulator/simexp/pcm/examples/performability/PerformabilityStrategyConstants.java
@@ -0,0 +1,8 @@
+package org.palladiosimulator.simexp.pcm.examples.performability;
+
+public class PerformabilityStrategyConstants {
+    
+    public static final String NODE_STATE_UNAVAILABLE = "unavailable";
+    public static final String NODE_STATE_AVAILABLE = "available";
+
+}
