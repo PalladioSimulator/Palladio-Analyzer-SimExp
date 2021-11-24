1f20bfa4f6ba058d19dba2a870a9ae4200f63747
Martina Rapp <rapp@fzi.de>
2021-11-24 17:41:42 +0100

    refactored classes to improve readability

--- bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/datasource/EDP2DataSource.java
+++ bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/datasource/EDP2DataSource.java
@@ -1,11 +1,9 @@
 package org.palladiosimulator.simexp.pcm.datasource;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import javax.measure.Measure;
 
@@ -16,98 +14,53 @@ import org.palladiosimulator.edp2.models.ExperimentData.Measurement;
 import org.palladiosimulator.edp2.models.ExperimentData.RawMeasurements;
 import org.palladiosimulator.measurementframework.measureprovider.IMeasureProvider;
 import org.palladiosimulator.metricspec.MetricDescription;
-import org.palladiosimulator.metricspec.MetricSetDescription;
 import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 import org.palladiosimulator.simexp.core.util.Pair;
 import org.palladiosimulator.simexp.pcm.state.PcmMeasurementSpecification;
 
-import com.google.common.collect.Maps;
-
 public class EDP2DataSource extends DataSource {
 
-	@Override
-	public MeasurementSeriesResult getSimulatedMeasurements(List<ExperimentRun> experimentRuns) {
-		return asDataSeries(filterStateMeasurements(experimentRuns));
-	}
+    @Override
+    public MeasurementSeriesResult getSimulatedMeasurements(List<ExperimentRun> experimentRuns) {
+        StateMeasurementFilter filter = new StateMeasurementFilter();
+        Map<PcmMeasurementSpecification, Measurement> filterStateMeasurements = filter
+            .filterStateMeasurements(experimentRuns);
+        return asDataSeries(filterStateMeasurements);
+    }
 
-	private Map<PcmMeasurementSpecification, Measurement> filterStateMeasurements(List<ExperimentRun> experimentRuns) {
-		Map<PcmMeasurementSpecification, Measurement> measurments = Maps.newHashMap();
-		for (ExperimentRun each : experimentRuns) {
-			Map<PcmMeasurementSpecification, Measurement> stateMeasurments = filterStateMeasurements(each);
-			measurments.putAll(stateMeasurments);
-		}
-		return measurments;
-	}
-	
-	private Map<PcmMeasurementSpecification, Measurement> filterStateMeasurements(ExperimentRun experimentRun) {
-		Map<PcmMeasurementSpecification, Measurement> specToMeas = new HashMap<>();
-		for (Measurement each : experimentRun.getMeasurement()) {
-			findSpecification(each).ifPresent(spec -> {
-				if (sameMetric(each, spec)) {
-					specToMeas.put(spec, each);
-				}
-			});
-		}
+    private MeasurementSeriesResult asDataSeries(Map<PcmMeasurementSpecification, Measurement> measurements) {
+        MeasurementSeriesResult result = new MeasurementSeriesResult();
+        for (PcmMeasurementSpecification each : measurements.keySet()) {
+            Measurement measure = measurements.get(each);
+            MetricDescription desc = each.getMetricDescription();
+            List<Pair<Number, Double>> measurementSeries = getMeasurementSeries(measure, desc);
+            result.addMeasurementSeries(each, measurementSeries);
+        }
+        return result;
+    }
 
-		// TODO exception handling
-		if (specToMeas.isEmpty()) {
-			throw new RuntimeException("");
-		}
-		return specToMeas;
-	}
+    private List<Pair<Number, Double>> getMeasurementSeries(Measurement measurement, MetricDescription metricDesc) {
+        List<Pair<Number, Double>> measurementSeries = new ArrayList<>();
+        Iterator<IMeasureProvider> iterator = getIterator(measurement);
+        while (iterator.hasNext()) {
+            IMeasureProvider provider = iterator.next();
+            Measure<?, ?> stateQuantity = provider.getMeasureForMetric(metricDesc);
+            Measure<?, ?> timeInstant = provider.getMeasureForMetric(MetricDescriptionConstants.POINT_IN_TIME_METRIC);
+            Object stateQuantityValue = stateQuantity.getValue();
+            if (stateQuantityValue instanceof Number) {
+                Number number = (Number) stateQuantityValue;
+                measurementSeries.add(Pair.of(number, (Double) timeInstant.getValue()));
+            }
+        }
+        return measurementSeries;
+    }
 
-	private Optional<PcmMeasurementSpecification> findSpecification(Measurement measurement) {
-		return getMeasurementSpecs().stream()
-				.filter(spec -> spec.hasMeasuringPoint(measurement.getMeasuringType().getMeasuringPoint())).findFirst();
-	}
-
-	private MeasurementSeriesResult asDataSeries(Map<PcmMeasurementSpecification, Measurement> measurements) {
-		MeasurementSeriesResult result = new MeasurementSeriesResult();
-		for (PcmMeasurementSpecification each : measurements.keySet()) {
-			Measurement measure = measurements.get(each);
-			MetricDescription desc = each.getMetricDescription();
-			result.addMeasurementSeries(each, getMeasurementSeries(measure, desc));
-		}
-		return result;
-	}
-
-	private boolean sameMetric(Measurement measurement, PcmMeasurementSpecification spec) {
-		MetricDescription first = spec.getMetricDescription();
-		MetricDescription second = measurement.getMeasuringType().getMetric();
-		if (second instanceof MetricSetDescription) {
-			return containsMetric(first, (MetricSetDescription) second);
-		}
-
-		return sameMetric(first, second);
-	}
-
-	private boolean containsMetric(MetricDescription first, MetricSetDescription second) {
-		return second.getSubsumedMetrics().stream().anyMatch(m -> sameMetric(first, m));
-	}
-
-	private boolean sameMetric(MetricDescription first, MetricDescription second) {
-		return first.getId().equals(second.getId());
-	}
-
-	private List<Pair<Number, Double>> getMeasurementSeries(Measurement measurement, MetricDescription metricDesc) {
-		List<Pair<Number, Double>> measurementSeries = new ArrayList<>();
-
-		Iterator<IMeasureProvider> iterator = getIterator(measurement);
-		while (iterator.hasNext()) {
-			IMeasureProvider provider = iterator.next();
-			Measure<?, ?> stateQuantity = provider.getMeasureForMetric(metricDesc);
-			Measure<?, ?> timeInstant = provider.getMeasureForMetric(MetricDescriptionConstants.POINT_IN_TIME_METRIC);
-			if (stateQuantity.getValue() instanceof Number) {
-				measurementSeries.add(Pair.of((Number) stateQuantity.getValue(), (Double) timeInstant.getValue()));
-			}
-		}
-
-		return measurementSeries;
-	}
-
-	private Iterator<IMeasureProvider> getIterator(Measurement measurement) {
-		RawMeasurements rawMeasurements = measurement.getMeasurementRanges().get(0).getRawMeasurements();
-		IDataSource dataSource = new Edp2DataTupleDataSource(rawMeasurements);
-		return dataSource.getDataStream().iterator();
-	}
+    private Iterator<IMeasureProvider> getIterator(Measurement measurement) {
+        RawMeasurements rawMeasurements = measurement.getMeasurementRanges()
+            .get(0)
+            .getRawMeasurements();
+        IDataSource dataSource = new Edp2DataTupleDataSource(rawMeasurements);
+        return dataSource.getDataStream()
+            .iterator();
+    }
 }
---
+++ bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/datasource/StateMeasurementFilter.java
@@ -0,0 +1,69 @@
+package org.palladiosimulator.simexp.pcm.datasource;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.eclipse.emf.common.util.EList;
+import org.palladiosimulator.edp2.models.ExperimentData.ExperimentRun;
+import org.palladiosimulator.edp2.models.ExperimentData.Measurement;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
+import org.palladiosimulator.simexp.core.entity.SimulatedMeasurementSpecification;
+import org.palladiosimulator.simexp.pcm.state.InitialPcmStateCreator;
+import org.palladiosimulator.simexp.pcm.state.PcmMeasurementSpecification;
+
+import com.google.common.collect.Maps;
+
+public class StateMeasurementFilter {
+    public Map<PcmMeasurementSpecification, Measurement> filterStateMeasurements(List<ExperimentRun> experimentRuns) {
+        Set<SimulatedMeasurementSpecification> initialMeasurementSpecs = InitialPcmStateCreator.getMeasurementSpecs();
+        return filterStateMeasurements(experimentRuns, initialMeasurementSpecs);
+    }
+    
+    private Map<PcmMeasurementSpecification, Measurement> filterStateMeasurements(List<ExperimentRun> experimentRuns, Set<SimulatedMeasurementSpecification> initialMeasurementSpecs) {
+        Map<PcmMeasurementSpecification, Measurement> measurments = Maps.newHashMap();
+        for (ExperimentRun each : experimentRuns) {
+            Map<PcmMeasurementSpecification, Measurement> stateMeasurments = filterStateMeasurements(each, initialMeasurementSpecs);
+            // TODO exception handling
+            if (stateMeasurments.isEmpty()) {
+                throw new RuntimeException("");
+            }
+            measurments.putAll(stateMeasurments);
+        }
+        return measurments;
+    }
+
+    private Map<PcmMeasurementSpecification, Measurement> filterStateMeasurements(ExperimentRun experimentRun, Set<SimulatedMeasurementSpecification> initialMeasurementSpecs) {
+        Map<PcmMeasurementSpecification, Measurement> specToMeas = new HashMap<>();
+        MetricComparer metricComparer = new MetricComparer();
+        EList<Measurement> measurements = experimentRun.getMeasurement();
+        for (Measurement each : measurements) {
+            List<PcmMeasurementSpecification> specifications = findSpecifications(each, initialMeasurementSpecs);
+            for (PcmMeasurementSpecification pcmSpec : specifications) {
+                if (metricComparer.sameMetric(each, pcmSpec)) {
+                    specToMeas.put(pcmSpec, each);
+                }
+            }
+        }
+        return specToMeas;
+    }
+
+    private List<PcmMeasurementSpecification> findSpecifications(Measurement measurement, Set<SimulatedMeasurementSpecification> initialMeasurementSpecs) {
+        MeasuringPoint measuringPoint = measurement.getMeasuringType()
+            .getMeasuringPoint();
+        Set<PcmMeasurementSpecification> measurementSpecs = getMeasurementSpecs(initialMeasurementSpecs);
+        return measurementSpecs.stream()
+            .filter(spec -> spec.hasMeasuringPoint(measuringPoint))
+            .collect(Collectors.toList());
+    }
+
+    private Set<PcmMeasurementSpecification> getMeasurementSpecs(
+            Set<SimulatedMeasurementSpecification> measurementSpecs) {
+        return measurementSpecs.stream()
+            .filter(PcmMeasurementSpecification.class::isInstance)
+            .map(PcmMeasurementSpecification.class::cast)
+            .collect(Collectors.toSet());
+    }
+}
