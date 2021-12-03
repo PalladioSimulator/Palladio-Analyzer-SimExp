96987e780cc5c6fb23b7b000a2dfcf92779f4b3c
Martina Rapp <rapp@fzi.de>
2021-12-03 14:16:01 +0100

    extracted strategies to separate classes

---
+++ bundles/org.palladiosimulator.simexp.pcm.examples.performability.loadbalancing/src/org/palladiosimulator/simexp/pcm/examples/performability/loadbalancing/LoadBalancingEmptyReconfigurationPlanningStrategy.java
@@ -0,0 +1,27 @@
+package org.palladiosimulator.simexp.pcm.examples.performability.loadbalancing;
+
+import java.util.Set;
+
+import org.palladiosimulator.simexp.core.strategy.SharedKnowledge;
+import org.palladiosimulator.simexp.markovian.model.markovmodel.markoventity.State;
+import org.palladiosimulator.simexp.pcm.action.QVToReconfiguration;
+import org.palladiosimulator.simexp.pcm.examples.performability.AbstractReconfigurationPlanningStrategy;
+import org.palladiosimulator.simexp.pcm.examples.performability.NodeRecoveryStrategy;
+import org.palladiosimulator.simexp.pcm.examples.performability.PerformabilityStrategyConfiguration;
+import org.palladiosimulator.simexp.pcm.examples.performability.PolicySelectionException;
+import org.palladiosimulator.simexp.pcm.state.PcmMeasurementSpecification;
+
+public class LoadBalancingEmptyReconfigurationPlanningStrategy extends AbstractReconfigurationPlanningStrategy{
+    
+    public LoadBalancingEmptyReconfigurationPlanningStrategy(PcmMeasurementSpecification responseTimeSpec,
+            PerformabilityStrategyConfiguration strategyConfiguration, NodeRecoveryStrategy recoveryStrategy) {
+        super(responseTimeSpec, strategyConfiguration, recoveryStrategy);
+    }
+
+    @Override
+    public QVToReconfiguration planReconfigurationSteps(State source, Set<QVToReconfiguration> options,
+            SharedKnowledge knowledge) throws PolicySelectionException {
+        return emptyReconfiguration();
+    }
+
+}
