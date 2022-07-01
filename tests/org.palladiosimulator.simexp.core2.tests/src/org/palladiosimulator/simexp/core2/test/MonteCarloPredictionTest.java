3acd57476a738d2e3f7df9d73a986ff51f980998
Martina Rapp <rapp@fzi.de>
2022-07-01 17:59:42 +0200

    changed tests to use original currentState implementation

--- tests/org.palladiosimulator.simexp.core2.tests/src/org/palladiosimulator/simexp/core2/test/MonteCarloPredictionTest.java
+++ tests/org.palladiosimulator.simexp.core2.tests/src/org/palladiosimulator/simexp/core2/test/MonteCarloPredictionTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -18,21 +17,15 @@ import org.palladiosimulator.simexp.core.valuefunction.ValueFunction;
 
 public class MonteCarloPredictionTest {
 
-
     private static final double DELTA = 1e-15;
+    private static final String RECONF_PATTERN = "#";
 
     private MonteCarloPrediction mcPrediction;
-    private ValueFunction valueFunction;
-//    private MonteCaroEstimator predictionEstimator;
-//    private IAccumulatedRewardManager accRewardManager;
+
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-//        accRewardManager = new AccumulatedRewardManager();
-        valueFunction = new ValueFunction();
-//        predictionEstimator = new FirstVisitMonteCarloEstimator(accRewardManager, valueFunction);
-//        mcPrediction = new MonteCarloPrediction(valueFunction, predictionEstimator);
         mcPrediction = MonteCarloPrediction.firstVisitEstimation();
     }
 
@@ -54,30 +47,35 @@ public class MonteCarloPredictionTest {
         SimulatedExperience sample3 = Mockito.mock(SimulatedExperience.class);
         SimulatedExperience sample4 = Mockito.mock(SimulatedExperience.class);
         SimulatedExperience sample5 = Mockito.mock(SimulatedExperience.class);
-        when(sample1.getReconfiguration()).thenReturn("A");
+        when(sample1.getId()).thenReturn("A_");
+        when(sample1.getReconfiguration()).thenReturn(RECONF_PATTERN);
         when(sample1.getReward()).thenReturn("3.0");
-        when(sample2.getReconfiguration()).thenReturn("A");
+        when(sample2.getId()).thenReturn("A_");
+        when(sample2.getReconfiguration()).thenReturn(RECONF_PATTERN);
         when(sample2.getReward()).thenReturn("2.0");
-        when(sample3.getReconfiguration()).thenReturn("B");
+        when(sample3.getId()).thenReturn("B_");
+        when(sample3.getReconfiguration()).thenReturn(RECONF_PATTERN);
         when(sample3.getReward()).thenReturn("-4.0");
-        when(sample4.getReconfiguration()).thenReturn("A");
+        when(sample4.getId()).thenReturn("A_");
+        when(sample4.getReconfiguration()).thenReturn(RECONF_PATTERN);
         when(sample4.getReward()).thenReturn("4.0");
-        when(sample5.getReconfiguration()).thenReturn("B");
+        when(sample5.getId()).thenReturn("B_");
+        when(sample5.getReconfiguration()).thenReturn(RECONF_PATTERN);
         when(sample5.getReward()).thenReturn("-3.0");
         List<SimulatedExperience> traj1 = Arrays.asList(sample1, sample2, sample3, sample4, sample5);
         
         // // Sampled trajectory2: ("B", -2), ("A", 3), ("B", -3), terminate
-//        StateAwareSimulatedExperience sample6 = Mockito.mock(StateAwareSimulatedExperience.class);
-//        StateAwareSimulatedExperience sample7 = Mockito.mock(StateAwareSimulatedExperience.class);
-//        StateAwareSimulatedExperience sample8 = Mockito.mock(StateAwareSimulatedExperience.class);
         SimulatedExperience sample6 = Mockito.mock(SimulatedExperience.class);
         SimulatedExperience sample7 = Mockito.mock(SimulatedExperience.class);
         SimulatedExperience sample8 = Mockito.mock(SimulatedExperience.class);
-        when(sample6.getReconfiguration()).thenReturn("A");
+        when(sample6.getId()).thenReturn("A_");
+        when(sample6.getReconfiguration()).thenReturn(RECONF_PATTERN);
         when(sample6.getReward()).thenReturn("3.0");
-        when(sample7.getReconfiguration()).thenReturn("A");
+        when(sample7.getId()).thenReturn("A_");
+        when(sample7.getReconfiguration()).thenReturn(RECONF_PATTERN);
         when(sample7.getReward()).thenReturn("2.0");
-        when(sample8.getReconfiguration()).thenReturn("B");
+        when(sample8.getId()).thenReturn("B_");
+        when(sample8.getReconfiguration()).thenReturn(RECONF_PATTERN);
         when(sample8.getReward()).thenReturn("-4.0");
         List<SimulatedExperience> traj2 = Arrays.asList(sample6, sample7, sample8);
         
@@ -87,6 +85,4 @@ public class MonteCarloPredictionTest {
         
         return sampleModel;
     }
-
-
 }
