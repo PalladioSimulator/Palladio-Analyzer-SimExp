1f18615ca0f743555c675829a216e07c21a49f31
Martina Rapp <rapp@fzi.de>
2023-01-17 17:41:16 +0100

    removed unused method; removed unnessary EMF package and factoy registration (is only required if not running in OSGI but standalone mode)

--- examples/org.palladiosimulator.simexp.pcm.examples/src/org/palladiosimulator/simexp/pcm/examples/executor/ExperimentRepositoryLoader.java
+++ examples/org.palladiosimulator.simexp.pcm.examples/src/org/palladiosimulator/simexp/pcm/examples/executor/ExperimentRepositoryLoader.java
@@ -5,19 +5,10 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.palladiosimulator.experimentautomation.application.tooladapter.simulizar.model.SimulizartooladapterPackage;
-import org.palladiosimulator.experimentautomation.experiments.Experiment;
 import org.palladiosimulator.experimentautomation.experiments.ExperimentRepository;
-import org.palladiosimulator.experimentautomation.experiments.ExperimentsPackage;
 
 public class ExperimentRepositoryLoader {
 
-	public ExperimentRepositoryLoader() {
-		registerFactories();
-	}
 	
 	/**
 	 * precondition:
@@ -36,38 +27,5 @@ public class ExperimentRepositoryLoader {
 	    return (ExperimentRepository) experiments.get(0);
 	}
 	
-	public Experiment loadExperiment(String file) {
-		ExperimentRepository loadExperimentRepository = loadExperimentRepository(file);
-        EList<Experiment> experiments = loadExperimentRepository.getExperiments();
-        Experiment exp = getFirstElement(experiments);
-		EcoreUtil.resolveAll(exp);
-		return exp;
-	}
 	
-	private ExperimentRepository loadExperimentRepository(String file) {
-		registerFactories();
-		ResourceSet rs = new ResourceSetImpl();
-		registerDefaultPackages(rs);
-		
-		URI uri = URI.createURI(file, true);
-        EList<EObject> experiments = rs.getResource(uri, true).getContents();
-		return (ExperimentRepository) getFirstElement(experiments);
-	}
-	
-	private void registerFactories() {
-		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-		reg.getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());       
-	}
-	
-	private void registerDefaultPackages(ResourceSet set) {
-		set.getPackageRegistry().put(ExperimentsPackage.eNS_URI, ExperimentsPackage.eINSTANCE);
-		set.getPackageRegistry().put(SimulizartooladapterPackage.eNS_URI, SimulizartooladapterPackage.eINSTANCE);
-	}
-	
-	private <T> T getFirstElement(EList<T> list) {
-		if (list.isEmpty()) {
-			throw new RuntimeException(String.format("The list of type %s shouldn't be empty.", list.getClass()));
-		}
-		return list.get(0);
-	}
 }
