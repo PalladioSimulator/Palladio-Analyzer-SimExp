e1b956acdfc340a9d3603b1a6a48b432d5e32d69
Ralf Sieger <sieger@fzi.de>
2023-12-31 14:34:06 +0100

    added missing generic types

--- bundles/org.palladiosimulator.simexp.pcm.reliability/src/org/palladiosimulator/simexp/pcm/reliability/process/PcmRelExperienceSimulationRunner.java
+++ bundles/org.palladiosimulator.simexp.pcm.reliability/src/org/palladiosimulator/simexp/pcm/reliability/process/PcmRelExperienceSimulationRunner.java
@@ -30,97 +30,121 @@ import tools.mdsd.probdist.api.factory.IProbabilityDistributionFactory;
 import tools.mdsd.probdist.api.factory.IProbabilityDistributionRegistry;
 import tools.mdsd.probdist.api.parser.ParameterParser;
 
-public class PcmRelExperienceSimulationRunner implements ExperienceSimulationRunner {
+public class PcmRelExperienceSimulationRunner<S, A> implements ExperienceSimulationRunner<S, A> {
 
-	private final UncertaintyBasedReliabilityPredictionConfig globalConfig;
-	private final DiscreteUncertaintyStateSpace uncertaintyStateSpace;
-	private final IProbabilityDistributionRegistry probabilityDistributionRegistry;
-	private final IProbabilityDistributionFactory probabilityDistributionFactory;
-	private final ParameterParser parameterParser;
-	private final IProbabilityDistributionRepositoryLookup probDistRepoLookup;
+    private final UncertaintyBasedReliabilityPredictionConfig globalConfig;
+    private final DiscreteUncertaintyStateSpace uncertaintyStateSpace;
+    private final IProbabilityDistributionRegistry probabilityDistributionRegistry;
+    private final IProbabilityDistributionFactory probabilityDistributionFactory;
+    private final ParameterParser parameterParser;
+    private final IProbabilityDistributionRepositoryLookup probDistRepoLookup;
 
-	public PcmRelExperienceSimulationRunner(UncertaintyBasedReliabilityPredictionConfig globalConfig, IProbabilityDistributionRegistry probabilityDistributionRegistry, IProbabilityDistributionFactory probabilityDistributionFactory, ParameterParser parameterParser, IProbabilityDistributionRepositoryLookup probDistRepoLookup) {
-		this.globalConfig = globalConfig;
-		this.uncertaintyStateSpace = buildUncertaintyStateSpace(globalConfig.getUncertaintyRepository(), parameterParser);
-		this.probabilityDistributionRegistry = probabilityDistributionRegistry;
-		this.probabilityDistributionFactory = probabilityDistributionFactory;
-		this.parameterParser = parameterParser;
-		this.probDistRepoLookup = probDistRepoLookup;
-	}
+    public PcmRelExperienceSimulationRunner(UncertaintyBasedReliabilityPredictionConfig globalConfig,
+            IProbabilityDistributionRegistry probabilityDistributionRegistry,
+            IProbabilityDistributionFactory probabilityDistributionFactory, ParameterParser parameterParser,
+            IProbabilityDistributionRepositoryLookup probDistRepoLookup) {
+        this.globalConfig = globalConfig;
+        this.uncertaintyStateSpace = buildUncertaintyStateSpace(globalConfig.getUncertaintyRepository(),
+                parameterParser);
+        this.probabilityDistributionRegistry = probabilityDistributionRegistry;
+        this.probabilityDistributionFactory = probabilityDistributionFactory;
+        this.parameterParser = parameterParser;
+        this.probDistRepoLookup = probDistRepoLookup;
+    }
 
-	private DiscreteUncertaintyStateSpace buildUncertaintyStateSpace(UncertaintyRepository uncertaintyRepo, ParameterParser parameterParser) {
-		var stateSpace = uncertaintyRepo.getUncertaintyInducedFailureTypes().stream()
-				.flatMap(each -> DiscreteUncertaintyStateSpace.valueSpaceOf(each, parameterParser).stream()).collect(toList());
-		return DiscreteUncertaintyStateSpace.of(stateSpace);
-	}
+    private DiscreteUncertaintyStateSpace buildUncertaintyStateSpace(UncertaintyRepository uncertaintyRepo,
+            ParameterParser parameterParser) {
+        var stateSpace = uncertaintyRepo.getUncertaintyInducedFailureTypes()
+            .stream()
+            .flatMap(each -> DiscreteUncertaintyStateSpace.valueSpaceOf(each, parameterParser)
+                .stream())
+            .collect(toList());
+        return DiscreteUncertaintyStateSpace.of(stateSpace);
+    }
 
-	@Override
-	public void simulate(SelfAdaptiveSystemState<?> sasState) {
-		if (PcmSelfAdaptiveSystemState.class.isInstance(sasState) == false) {
-			// TODO Exception handling
-			throw new RuntimeException();
-		}
+    @Override
+    public void simulate(SelfAdaptiveSystemState<S, A> sasState) {
+        if (PcmSelfAdaptiveSystemState.class.isInstance(sasState) == false) {
+            // TODO Exception handling
+            throw new RuntimeException();
+        }
 
-		var result = makePrediction((PcmSelfAdaptiveSystemState) sasState, probabilityDistributionRegistry, probabilityDistributionFactory, parameterParser, probDistRepoLookup);
-		retrieveAndSetStateQuantities(sasState.getQuantifiedState(), result);
-	}
+        var result = makePrediction((PcmSelfAdaptiveSystemState<A>) sasState, probabilityDistributionRegistry,
+                probabilityDistributionFactory, parameterParser, probDistRepoLookup);
+        retrieveAndSetStateQuantities(sasState.getQuantifiedState(), result);
+    }
 
-	private ReliabilityPredictionResult makePrediction(PcmSelfAdaptiveSystemState pcmState, IProbabilityDistributionRegistry probabilityDistributionRegistry, IProbabilityDistributionFactory probabilityDistributionFactory, ParameterParser parameterParser, IProbabilityDistributionRepositoryLookup probDistRepoLookup) {
-		var config = deriveConfigFrom(pcmState);
-		var uncertaintyStates = deriveUncertaintyStates(pcmState.getPerceivedEnvironmentalState());
-		return UncertaintyBasedReliabilityPrediction.predictGiven(uncertaintyStates, config, probabilityDistributionRegistry, probabilityDistributionFactory, parameterParser, probDistRepoLookup);
-	}
+    private ReliabilityPredictionResult makePrediction(PcmSelfAdaptiveSystemState<A> pcmState,
+            IProbabilityDistributionRegistry probabilityDistributionRegistry,
+            IProbabilityDistributionFactory probabilityDistributionFactory, ParameterParser parameterParser,
+            IProbabilityDistributionRepositoryLookup probDistRepoLookup) {
+        var config = deriveConfigFrom(pcmState);
+        var uncertaintyStates = deriveUncertaintyStates(pcmState.getPerceivedEnvironmentalState());
+        return UncertaintyBasedReliabilityPrediction.predictGiven(uncertaintyStates, config,
+                probabilityDistributionRegistry, probabilityDistributionFactory, parameterParser, probDistRepoLookup);
+    }
 
-	private UncertaintyBasedReliabilityPredictionConfig deriveConfigFrom(PcmSelfAdaptiveSystemState pcmState) {
-		var blackboard = prepareBlackboardForAnalysis(pcmState);
-		return UncertaintyBasedReliabilityPredictionConfig.newBuilder().rebuild(globalConfig, blackboard);
-	}
+    private UncertaintyBasedReliabilityPredictionConfig deriveConfigFrom(PcmSelfAdaptiveSystemState<A> pcmState) {
+        var blackboard = prepareBlackboardForAnalysis(pcmState);
+        return UncertaintyBasedReliabilityPredictionConfig.newBuilder()
+            .rebuild(globalConfig, blackboard);
+    }
 
-	private MDSDBlackboard prepareBlackboardForAnalysis(PcmSelfAdaptiveSystemState pcmState) {
-		var pcm = pcmState.getArchitecturalConfiguration().getConfiguration();
-		var builderJob = new PrepareBlackboardForReliabilityAnalysisJob(pcm);
-		try {
-			builderJob.execute(new NullProgressMonitor());
-		} catch (JobFailedException | UserCanceledException e) {
-			throw new RuntimeException("Something went wrong while preparing the blackboard for analysis.", e);
-		}
-		return builderJob.getBlackboard();
-	}
+    private MDSDBlackboard prepareBlackboardForAnalysis(PcmSelfAdaptiveSystemState<A> pcmState) {
+        var pcm = pcmState.getArchitecturalConfiguration()
+            .getConfiguration();
+        var builderJob = new PrepareBlackboardForReliabilityAnalysisJob(pcm);
+        try {
+            builderJob.execute(new NullProgressMonitor());
+        } catch (JobFailedException | UserCanceledException e) {
+            throw new RuntimeException("Something went wrong while preparing the blackboard for analysis.", e);
+        }
+        return builderJob.getBlackboard();
+    }
 
-	private void retrieveAndSetStateQuantities(StateQuantity quantity, ReliabilityPredictionResult result) {
-		filterPcmRelMeasurementsSpec(quantity).forEach(spec -> {
-			var probOfSuccess = result.filterPredictionResultsFor(spec.getUsageScenario()).iterator().next();
-			quantity.setMeasurement(probOfSuccess.getConditionalProbabilityOfSuccess(), spec);
-		});
-	}
+    private void retrieveAndSetStateQuantities(StateQuantity quantity, ReliabilityPredictionResult result) {
+        filterPcmRelMeasurementsSpec(quantity).forEach(spec -> {
+            var probOfSuccess = result.filterPredictionResultsFor(spec.getUsageScenario())
+                .iterator()
+                .next();
+            quantity.setMeasurement(probOfSuccess.getConditionalProbabilityOfSuccess(), spec);
+        });
+    }
 
-	private List<UncertaintyState> deriveUncertaintyStates(PerceivableEnvironmentalState envState) {
-		List<UncertaintyState> uncertaintyStateTuple = Lists.newArrayList();
-		for (InputValue each : toInputs(envState)) {
-			uncertaintyStateSpace.findStateInstantiating(each.variable.getInstantiatedTemplate()).ifPresent(s -> {
-				UncertaintyState stateToAdd = s.newValuedStateWith(each.asCategorical());
-				uncertaintyStateTuple.add(stateToAdd);
-			});
-		}
-		return uncertaintyStateTuple;
-	}
+    private List<UncertaintyState> deriveUncertaintyStates(PerceivableEnvironmentalState envState) {
+        List<UncertaintyState> uncertaintyStateTuple = Lists.newArrayList();
+        for (InputValue each : toInputs(envState)) {
+            uncertaintyStateSpace.findStateInstantiating(each.variable.getInstantiatedTemplate())
+                .ifPresent(s -> {
+                    UncertaintyState stateToAdd = s.newValuedStateWith(each.asCategorical());
+                    uncertaintyStateTuple.add(stateToAdd);
+                });
+        }
+        return uncertaintyStateTuple;
+    }
 
-	private List<InputValue> toInputs(PerceivableEnvironmentalState envState) {
-		Object sample = envState.getValue().getValue();
-		if (List.class.isInstance(sample)) {
-			List<?> inputs = List.class.cast(sample);
-			if (inputs.isEmpty() == false) {
-				if (InputValue.class.isInstance(inputs.get(0))) {
-					return inputs.stream().map(InputValue.class::cast).collect(toList());
-				}
-			}
-		}
-		return Lists.newArrayList();
-	}
+    private List<InputValue> toInputs(PerceivableEnvironmentalState envState) {
+        Object sample = envState.getValue()
+            .getValue();
+        if (List.class.isInstance(sample)) {
+            List<?> inputs = List.class.cast(sample);
+            if (inputs.isEmpty() == false) {
+                if (InputValue.class.isInstance(inputs.get(0))) {
+                    return inputs.stream()
+                        .map(InputValue.class::cast)
+                        .collect(toList());
+                }
+            }
+        }
+        return Lists.newArrayList();
+    }
 
-	private List<PcmRelSimulatedMeasurementSpec> filterPcmRelMeasurementsSpec(StateQuantity quantity) {
-		return quantity.getMeasurementSpecs().stream().filter(PcmRelSimulatedMeasurementSpec.class::isInstance)
-				.map(PcmRelSimulatedMeasurementSpec.class::cast).collect(toList());
-	}
+    private List<PcmRelSimulatedMeasurementSpec> filterPcmRelMeasurementsSpec(StateQuantity quantity) {
+        return quantity.getMeasurementSpecs()
+            .stream()
+            .filter(PcmRelSimulatedMeasurementSpec.class::isInstance)
+            .map(PcmRelSimulatedMeasurementSpec.class::cast)
+            .collect(toList());
+    }
 
 }
