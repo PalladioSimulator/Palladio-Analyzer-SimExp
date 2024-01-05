885e9fe0b823e4583dea68df9ba539ee76fa89dc
Ralf Sieger <sieger@fzi.de>
2024-01-05 12:45:48 +0100

    added missing generic types

--- bundles/org.palladiosimulator.simexp.pcm.prism/src/org/palladiosimulator/simexp/pcm/prism/generator/PrismGenerator.java
+++ bundles/org.palladiosimulator.simexp.pcm.prism/src/org/palladiosimulator/simexp/pcm/prism/generator/PrismGenerator.java
@@ -4,7 +4,7 @@ import org.palladiosimulator.simexp.pcm.prism.entity.PrismContext;
 import org.palladiosimulator.simexp.pcm.prism.entity.PrismSimulatedMeasurementSpec;
 import org.palladiosimulator.simexp.pcm.state.PcmSelfAdaptiveSystemState;
 
-public interface PrismGenerator {
-	
-	public PrismContext generate(PcmSelfAdaptiveSystemState sasState, PrismSimulatedMeasurementSpec prismSpec);
+public interface PrismGenerator<A> {
+
+    public PrismContext generate(PcmSelfAdaptiveSystemState<A> sasState, PrismSimulatedMeasurementSpec prismSpec);
 }
