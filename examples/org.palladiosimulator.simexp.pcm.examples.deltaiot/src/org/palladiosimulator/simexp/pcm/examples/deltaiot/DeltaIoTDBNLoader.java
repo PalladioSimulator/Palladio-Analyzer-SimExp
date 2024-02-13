8298826dda18880adea2994ad668f75c5f3198e6
Ralf Sieger <sieger@fzi.de>
2024-02-13 10:19:22 +0100

    added generic type to BayesianNetwork

--- examples/org.palladiosimulator.simexp.pcm.examples.deltaiot/src/org/palladiosimulator/simexp/pcm/examples/deltaiot/DeltaIoTDBNLoader.java
+++ examples/org.palladiosimulator.simexp.pcm.examples.deltaiot/src/org/palladiosimulator/simexp/pcm/examples/deltaiot/DeltaIoTDBNLoader.java
@@ -14,49 +14,59 @@ import org.palladiosimulator.envdyn.environment.dynamicmodel.DynamicBehaviourExt
 import org.palladiosimulator.envdyn.environment.staticmodel.GroundProbabilisticNetwork;
 import org.palladiosimulator.envdyn.environment.templatevariable.TemplatevariablePackage;
 
+import tools.mdsd.probdist.api.entity.CategoricalValue;
+
 public class DeltaIoTDBNLoader {
 
-	private final static String DELTAIOT_PATH = "/org.palladiosimulator.envdyn.examples.deltaiot";
-	private final static String STATIC_MODEL_EXTENSION = "staticmodel";
-	private final static String DYNAMIC_MODEL_EXTENSION = "dynamicmodel";
-	private final static String BN_FILE = String.format("%1s/model/%2s.%3s", DELTAIOT_PATH,
-			"DeltaIoTNonTemporalEnvironment", STATIC_MODEL_EXTENSION);
-	private final static String DBN_FILE = String.format("%1s/model/%2s.%3s", DELTAIOT_PATH,
-			"DeltaIoTEnvironmentalDynamics", DYNAMIC_MODEL_EXTENSION);
-	private final static URI DBN_URI = URI.createPlatformResourceURI(DBN_FILE, true);
-	private final static URI BN_URI = URI.createPlatformResourceURI(BN_FILE, true);
-	private final static ResourceSet RESOURCE_SET = new ResourceSetImpl();
-	static {
-		RESOURCE_SET.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
-		RESOURCE_SET.getPackageRegistry().put(TemplatevariablePackage.eNS_URI, TemplatevariablePackage.eINSTANCE);
-	}
+    private final static String DELTAIOT_PATH = "/org.palladiosimulator.envdyn.examples.deltaiot";
+    private final static String STATIC_MODEL_EXTENSION = "staticmodel";
+    private final static String DYNAMIC_MODEL_EXTENSION = "dynamicmodel";
+    private final static String BN_FILE = String.format("%1s/model/%2s.%3s", DELTAIOT_PATH,
+            "DeltaIoTNonTemporalEnvironment", STATIC_MODEL_EXTENSION);
+    private final static String DBN_FILE = String.format("%1s/model/%2s.%3s", DELTAIOT_PATH,
+            "DeltaIoTEnvironmentalDynamics", DYNAMIC_MODEL_EXTENSION);
+    private final static URI DBN_URI = URI.createPlatformResourceURI(DBN_FILE, true);
+    private final static URI BN_URI = URI.createPlatformResourceURI(BN_FILE, true);
+    private final static ResourceSet RESOURCE_SET = new ResourceSetImpl();
+    static {
+        RESOURCE_SET.getResourceFactoryRegistry()
+            .getExtensionToFactoryMap()
+            .put("*", new XMIResourceFactoryImpl());
+        RESOURCE_SET.getPackageRegistry()
+            .put(TemplatevariablePackage.eNS_URI, TemplatevariablePackage.eINSTANCE);
+    }
 
-	public static void persist(EObject eObj, String path) {
-		Resource resource = RESOURCE_SET.createResource(URI.createFileURI(path));
-		resource.getContents().add(eObj);
-		try {
-			resource.save(Collections.EMPTY_MAP);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void persist(EObject eObj, String path) {
+        Resource resource = RESOURCE_SET.createResource(URI.createFileURI(path));
+        resource.getContents()
+            .add(eObj);
+        try {
+            resource.save(Collections.EMPTY_MAP);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static DynamicBayesianNetwork loadDBN() {
+    public static DynamicBayesianNetwork<CategoricalValue> loadDBN() {
 //		BayesianNetwork bn = new BayesianNetwork(null, loadGroundProbabilisticNetwork());
 //		return new DynamicBayesianNetwork(null, bn, loadDynamicBehaviourExtension());
-		return null;
-	}
+        return null;
+    }
 
 //	private static DynamicBehaviourExtension loadDynamicBehaviourExtension() {
 //		return loadDynamicBehaviourRepo().getExtensions().get(0);
 //	}
 
-	private static DynamicBehaviourExtension loadDynamicBehaviourExtension() {
-		return DynamicBehaviourExtension.class.cast(RESOURCE_SET.getResource(DBN_URI, true).getContents().get(0));
-	}
+    private static DynamicBehaviourExtension loadDynamicBehaviourExtension() {
+        return DynamicBehaviourExtension.class.cast(RESOURCE_SET.getResource(DBN_URI, true)
+            .getContents()
+            .get(0));
+    }
 
-	public static GroundProbabilisticNetwork loadGroundProbabilisticNetwork() {
-		return GroundProbabilisticNetwork.class.cast(RESOURCE_SET.getResource(BN_URI, true).getContents().get(0));
-	}
+    public static GroundProbabilisticNetwork loadGroundProbabilisticNetwork() {
+        return GroundProbabilisticNetwork.class.cast(RESOURCE_SET.getResource(BN_URI, true)
+            .getContents()
+            .get(0));
+    }
 
 }
