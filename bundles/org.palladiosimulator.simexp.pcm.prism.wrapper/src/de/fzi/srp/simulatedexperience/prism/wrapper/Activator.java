54f6962eb1c9e033196ba3e9d77401fce0189540
Ralf Sieger <sieger@fzi.de>
2024-02-19 09:59:16 +0100

    merged with master

--- bundles/org.palladiosimulator.simexp.pcm.prism.wrapper/src/de/fzi/srp/simulatedexperience/prism/wrapper/Activator.java
+++ bundles/org.palladiosimulator.simexp.pcm.prism.wrapper/src/de/fzi/srp/simulatedexperience/prism/wrapper/Activator.java
@@ -11,30 +11,33 @@ import org.palladiosimulator.simexp.service.registry.ServiceRegistry;
 import de.fzi.srp.simulatedexperience.prism.wrapper.service.PrismInvocationService;
 
 public class Activator implements BundleActivator {
-	private static BundleContext context;
+    private static BundleContext context;
 
-	static BundleContext getContext() {
-		return context;
-	}
+    static BundleContext getContext() {
+        return context;
+    }
 
-	@Override
-	public void start(BundleContext bundleContext) throws Exception {
-		ILog logger = Platform.getLog(getClass());
-		Activator.context = bundleContext;
-		
-		String prism = System.getenv("PRISM");
-		if (prism == null) {
-			// In the launcher under Environment add environment variable 'PATH' with '${env_var:PRISM}\lib'
-			// See https://github.com/prismmodelchecker/prism/wiki/Setting-up-Eclipse
-			logger.error("environment variable 'PRISM' not found -> unable to register PrismService");
-		} else {
-			ServiceRegistry.registerService(ServiceEntry.service(PrismService.class).isProvidedBy(PrismInvocationService.class));
-		}
-	}
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        ILog logger = Platform.getLog(getClass());
+        Activator.context = bundleContext;
 
-	@Override
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
+        String prism = System.getenv("PRISM");
+        if (prism == null) {
+            // In the launcher under Environment add environment variable:
+            // WINDOWS: 'PATH' with '${env_var:PRISM}\lib'
+            // LINUX: 'LD_LIBRARY_PATH' with '${env_var:PRISM}/lib'
+            // See https://github.com/prismmodelchecker/prism/wiki/Setting-up-Eclipse
+            logger.error("environment variable 'PRISM' not found -> unable to register PrismService");
+        } else {
+            ServiceRegistry.registerService(ServiceEntry.service(PrismService.class)
+                .isProvidedBy(PrismInvocationService.class));
+        }
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        Activator.context = null;
+    }
 
 }
