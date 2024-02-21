e58f0a8f59981c69be353b795aeaced1eb350dba
Martina Rapp <rapp@fzi.de>
2024-02-21 10:30:50 +0100

    added extension point for service providers

--- bundles/org.palladiosimulator.simexp.pcm.prism.wrapper/src/de/fzi/srp/simulatedexperience/prism/wrapper/Activator.java
+++ bundles/org.palladiosimulator.simexp.pcm.prism.wrapper/src/de/fzi/srp/simulatedexperience/prism/wrapper/Activator.java
@@ -1,14 +1,7 @@
 package de.fzi.srp.simulatedexperience.prism.wrapper;
 
-import org.eclipse.core.runtime.ILog;
-import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.palladiosimulator.simexp.pcm.prism.service.PrismService;
-import org.palladiosimulator.simexp.service.registry.ServiceEntry;
-import org.palladiosimulator.simexp.service.registry.ServiceRegistry;
-
-import de.fzi.srp.simulatedexperience.prism.wrapper.service.PrismInvocationService;
 
 public class Activator implements BundleActivator {
     private static BundleContext context;
@@ -19,20 +12,7 @@ public class Activator implements BundleActivator {
 
     @Override
     public void start(BundleContext bundleContext) throws Exception {
-        ILog logger = Platform.getLog(getClass());
         Activator.context = bundleContext;
-
-        String prism = System.getenv("PRISM");
-        if (prism == null) {
-            // In the launcher under Environment add environment variable:
-            // WINDOWS: 'PATH' with '${env_var:PRISM}\lib'
-            // LINUX: 'LD_LIBRARY_PATH' with '${env_var:PRISM}/lib'
-            // See https://github.com/prismmodelchecker/prism/wiki/Setting-up-Eclipse
-            logger.error("environment variable 'PRISM' not found -> unable to register PrismService");
-        } else {
-            ServiceRegistry.registerService(ServiceEntry.service(PrismService.class)
-                .isProvidedBy(PrismInvocationService.class));
-        }
     }
 
     @Override
