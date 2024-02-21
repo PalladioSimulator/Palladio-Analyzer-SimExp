b7e1ec27293b12c9654fc08df81ea2a576ceb493
Martina Rapp <rapp@fzi.de>
2024-02-21 10:44:57 +0100

    switched to extension point registration for service providers

---
+++ bundles/org.palladiosimulator.simexp.distribution.apache/src/org/palladiosimulator/simexp/distribution/apache/factory/ApacheServiceProvider.java
@@ -0,0 +1,15 @@
+package org.palladiosimulator.simexp.distribution.apache.factory;
+
+import org.palladiosimulator.simexp.distribution.factory.ProbabilityDistributionFactory;
+import org.palladiosimulator.simexp.service.registry.ServiceEntry;
+import org.palladiosimulator.simexp.service.registry.ServiceProvider;
+import org.palladiosimulator.simexp.service.registry.ServiceRegistration;
+
+public class ApacheServiceProvider implements ServiceProvider {
+
+    @Override
+    public void register(ServiceRegistration serviceRegistration) {
+        serviceRegistration.registerAService(ServiceEntry.service(ProbabilityDistributionFactory.class)
+            .isProvidedBy(ProbabilityDistributionFactoryAdapter.class));
+    }
+}
