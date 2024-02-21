cd6478818afd49be4089b38f51e9f4ba6b125e4f
Martina Rapp <rapp@fzi.de>
2024-02-21 11:21:49 +0100

    switched to log4j logging

--- bundles/org.palladiosimulator.simexp.pcm.prism.wrapper/src/de/fzi/srp/simulatedexperience/prism/wrapper/service/PrismServiceProvider.java
+++ bundles/org.palladiosimulator.simexp.pcm.prism.wrapper/src/de/fzi/srp/simulatedexperience/prism/wrapper/service/PrismServiceProvider.java
@@ -1,25 +1,23 @@
 package de.fzi.srp.simulatedexperience.prism.wrapper.service;
 
-import org.eclipse.core.runtime.ILog;
-import org.eclipse.core.runtime.Platform;
+import org.apache.log4j.Logger;
 import org.palladiosimulator.simexp.pcm.prism.service.PrismService;
 import org.palladiosimulator.simexp.service.registry.ServiceEntry;
 import org.palladiosimulator.simexp.service.registry.ServiceProvider;
 import org.palladiosimulator.simexp.service.registry.ServiceRegistration;
 
 public class PrismServiceProvider implements ServiceProvider {
+    private static final Logger LOGGER = Logger.getLogger(PrismServiceProvider.class);
 
     @Override
     public void register(ServiceRegistration serviceRegistration) {
-        ILog logger = Platform.getLog(getClass());
-
         String prism = System.getenv("PRISM");
         if (prism == null) {
             // In the launcher under Environment add environment variable:
             // WINDOWS: 'PATH' with '${env_var:PRISM}\lib'
             // LINUX: 'LD_LIBRARY_PATH' with '${env_var:PRISM}/lib'
             // See https://github.com/prismmodelchecker/prism/wiki/Setting-up-Eclipse
-            logger.error("environment variable 'PRISM' not found -> unable to register PrismService");
+            LOGGER.error("environment variable 'PRISM' not found -> unable to register PrismService");
         } else {
             serviceRegistration.registerService(ServiceEntry.service(PrismService.class)
                 .isProvidedBy(PrismInvocationService.class));
