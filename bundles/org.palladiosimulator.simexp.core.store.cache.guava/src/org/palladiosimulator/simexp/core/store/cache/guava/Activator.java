5906a9f1404c15d5468cf2d0cc41d5e7824d294f
Martina Rapp <rapp@fzi.de>
2024-02-21 10:44:57 +0100

    switched to extension point registration for service providers

--- bundles/org.palladiosimulator.simexp.core.store.cache.guava/src/org/palladiosimulator/simexp/core/store/cache/guava/Activator.java
+++ bundles/org.palladiosimulator.simexp.core.store.cache.guava/src/org/palladiosimulator/simexp/core/store/cache/guava/Activator.java
@@ -2,35 +2,34 @@ package org.palladiosimulator.simexp.core.store.cache.guava;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.palladiosimulator.simexp.core.store.SimulatedExperienceCache;
-import org.palladiosimulator.simexp.core.store.cache.guava.loader.GuavaSimulatedExperienceCache;
-import org.palladiosimulator.simexp.service.registry.ServiceEntry;
-import org.palladiosimulator.simexp.service.registry.ServiceRegistry;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+    private static BundleContext context;
 
-	static BundleContext getContext() {
-		return context;
-	}
+    static BundleContext getContext() {
+        return context;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-		
-		ServiceRegistry.registerService(ServiceEntry.service(SimulatedExperienceCache.class).isProvidedBy(GuavaSimulatedExperienceCache.class));
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        Activator.context = bundleContext;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        Activator.context = null;
+    }
 
 }
