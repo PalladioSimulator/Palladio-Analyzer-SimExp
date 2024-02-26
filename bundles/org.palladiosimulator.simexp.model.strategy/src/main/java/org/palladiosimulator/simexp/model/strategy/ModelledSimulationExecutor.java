883d15beb0f6ca2ff9508538c7bd574398397301
Martina Rapp <rapp@fzi.de>
2024-02-26 13:55:21 +0100

    added kmodel types

--- bundles/org.palladiosimulator.simexp.model.strategy/src/main/java/org/palladiosimulator/simexp/model/strategy/ModelledSimulationExecutor.java
+++ bundles/org.palladiosimulator.simexp.model.strategy/src/main/java/org/palladiosimulator/simexp/model/strategy/ModelledSimulationExecutor.java
@@ -1,21 +1,24 @@
-package org.palladiosimulator.simexp.pcm.examples.executor;
+package org.palladiosimulator.simexp.model.strategy;
 
 import org.palladiosimulator.experimentautomation.experiments.Experiment;
 import org.palladiosimulator.simexp.core.evaluation.TotalRewardCalculation;
 import org.palladiosimulator.simexp.core.process.ExperienceSimulator;
 import org.palladiosimulator.simexp.dsl.kmodel.kmodel.Kmodel;
 import org.palladiosimulator.simexp.markovian.activity.Policy;
-import org.palladiosimulator.simexp.markovian.model.markovmodel.markoventity.Action;
 import org.palladiosimulator.simexp.pcm.action.IQVToReconfigurationManager;
+import org.palladiosimulator.simexp.pcm.action.QVToReconfiguration;
+import org.palladiosimulator.simexp.pcm.examples.executor.PcmExperienceSimulationExecutor;
 import org.palladiosimulator.simexp.pcm.util.IExperimentProvider;
 import org.palladiosimulator.simexp.pcm.util.SimulationParameters;
+import org.palladiosimulator.simulizar.reconfiguration.qvto.QVTOReconfigurator;
 
-public abstract class KmodelSimulationExecutor<C, A, Aa extends Action<A>, R>
-        extends PcmExperienceSimulationExecutor<C, A, Aa, R> {
+public abstract class ModelledSimulationExecutor<C, R>
+        extends PcmExperienceSimulationExecutor<C, QVTOReconfigurator, QVToReconfiguration, R> {
     private final Kmodel kmodel;
 
-    protected KmodelSimulationExecutor(ExperienceSimulator<C, A, R> experienceSimulator, Experiment experiment,
-            SimulationParameters simulationParameters, Policy<A, Aa> reconfSelectionPolicy,
+    protected ModelledSimulationExecutor(ExperienceSimulator<C, QVTOReconfigurator, R> experienceSimulator,
+            Experiment experiment, SimulationParameters simulationParameters,
+            Policy<QVTOReconfigurator, QVToReconfiguration> reconfSelectionPolicy,
             TotalRewardCalculation rewardCalculation, IExperimentProvider experimentProvider,
             IQVToReconfigurationManager qvtoReconfigurationManager, Kmodel kmodel) {
         super(experienceSimulator, experiment, simulationParameters, reconfSelectionPolicy, rewardCalculation,
