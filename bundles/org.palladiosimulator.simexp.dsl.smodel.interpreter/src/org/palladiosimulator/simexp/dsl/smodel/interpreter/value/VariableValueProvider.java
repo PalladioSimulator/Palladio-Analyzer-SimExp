933bfe156878abc3b62b7f22507ffb4fcebf8298
Ralf Sieger <sieger@fzi.de>
2024-04-11 09:13:12 +0200

    added remaining literal supports

--- bundles/org.palladiosimulator.simexp.dsl.smodel.interpreter/src/org/palladiosimulator/simexp/dsl/smodel/interpreter/value/VariableValueProvider.java
+++ bundles/org.palladiosimulator.simexp.dsl.smodel.interpreter/src/org/palladiosimulator/simexp/dsl/smodel/interpreter/value/VariableValueProvider.java
@@ -7,6 +7,7 @@ import org.palladiosimulator.simexp.dsl.smodel.interpreter.ExpressionCalculator;
 import org.palladiosimulator.simexp.dsl.smodel.interpreter.IFieldValueProvider;
 import org.palladiosimulator.simexp.dsl.smodel.smodel.Expression;
 import org.palladiosimulator.simexp.dsl.smodel.smodel.Field;
+import org.palladiosimulator.simexp.dsl.smodel.smodel.StringLiteral;
 import org.palladiosimulator.simexp.dsl.smodel.smodel.Variable;
 
 public class VariableValueProvider implements IFieldValueProvider {
@@ -14,8 +15,8 @@ public class VariableValueProvider implements IFieldValueProvider {
     private final ExpressionCalculator expressionCalculator;
     private final Map<Variable, Object> variableValueMap;
 
-    public VariableValueProvider(ExpressionCalculator expressionCalculator) {
-        this.expressionCalculator = expressionCalculator;
+    public VariableValueProvider(IFieldValueProvider fieldValueProvider) {
+        this.expressionCalculator = new ExpressionCalculator(fieldValueProvider);
         this.variableValueMap = new HashMap<>();
     }
 
@@ -34,16 +35,41 @@ public class VariableValueProvider implements IFieldValueProvider {
 
     @Override
     public Double getDoubleValue(Field field) {
-        return null;
+        Variable variable = (Variable) field;
+        Object currentValue = variableValueMap.get(variable);
+        if (currentValue != null) {
+            return (Double) currentValue;
+        }
+        Expression value = variable.getValue();
+        Double calculatedValue = expressionCalculator.calculateDouble(value);
+        variableValueMap.put(variable, calculatedValue);
+        return calculatedValue;
     }
 
     @Override
     public Integer getIntegerValue(Field field) {
-        return null;
+        Variable variable = (Variable) field;
+        Object currentValue = variableValueMap.get(variable);
+        if (currentValue != null) {
+            return (Integer) currentValue;
+        }
+        Expression value = variable.getValue();
+        Integer calculatedValue = expressionCalculator.calculateInteger(value);
+        variableValueMap.put(variable, calculatedValue);
+        return calculatedValue;
     }
 
     @Override
     public String getStringValue(Field field) {
-        return null;
+        Variable variable = (Variable) field;
+        Object currentValue = variableValueMap.get(variable);
+        if (currentValue != null) {
+            return (String) currentValue;
+        }
+        Expression value = variable.getValue();
+        StringLiteral literal = (StringLiteral) value.getLiteral();
+        String calculatedValue = literal.getValue();
+        variableValueMap.put(variable, calculatedValue);
+        return calculatedValue;
     }
 }
--- tests/org.palladiosimulator.simexp.dsl.smodel.interpreter.tests/src/org/palladiosimulator/simexp/dsl/smodel/interpreter/value/VariableValueProviderTest.java
+++ tests/org.palladiosimulator.simexp.dsl.smodel.interpreter.tests/src/org/palladiosimulator/simexp/dsl/smodel/interpreter/value/VariableValueProviderTest.java
@@ -1,37 +1,68 @@
 package org.palladiosimulator.simexp.dsl.smodel.interpreter.value;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.offset;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import org.junit.Test;
 import org.mockito.Mock;
-import org.palladiosimulator.simexp.dsl.smodel.interpreter.ExpressionCalculator;
+import org.palladiosimulator.simexp.dsl.smodel.interpreter.IFieldValueProvider;
 import org.palladiosimulator.simexp.dsl.smodel.smodel.DataType;
 import org.palladiosimulator.simexp.dsl.smodel.smodel.Variable;
 import org.palladiosimulator.simexp.dsl.smodel.test.util.SmodelCreator;
 
 public class VariableValueProviderTest {
+    private static final double DOUBLE_EPSILON = 1e-15;
+
     private VariableValueProvider provider;
 
     @Mock
-    private ExpressionCalculator expressionCalculator;
+    private IFieldValueProvider fieldValueProvider;
 
     private SmodelCreator smodelCreator;
 
     public VariableValueProviderTest() {
         initMocks(this);
         smodelCreator = new SmodelCreator();
-        provider = new VariableValueProvider(expressionCalculator);
+        provider = new VariableValueProvider(fieldValueProvider);
     }
 
     @Test
     public void boolLiteral() {
-        Variable variable = smodelCreator.createVariable("const", DataType.BOOL, smodelCreator.createBoolLiteral(true));
-        when(expressionCalculator.calculateBoolean(variable.getValue())).thenReturn(true);
+        Variable variable = smodelCreator.createVariable("variable", DataType.BOOL,
+                smodelCreator.createBoolLiteral(true));
 
         Boolean actualValue = provider.getBoolValue(variable);
 
         assertThat(actualValue).isTrue();
+    }
+
+    @Test
+    public void intLiteral() {
+        Variable variable = smodelCreator.createVariable("variable", DataType.INT, smodelCreator.createIntLiteral(1));
+
+        Integer actualValue = provider.getIntegerValue(variable);
+
+        assertThat(actualValue).isEqualTo(1);
+    }
+
+    @Test
+    public void doubleLiteral() {
+        Variable variable = smodelCreator.createVariable("variable", DataType.DOUBLE,
+                smodelCreator.createDoubleLiteral(1.0));
+
+        Double actualValue = provider.getDoubleValue(variable);
+
+        assertThat(actualValue).isEqualTo(1.0, offset(DOUBLE_EPSILON));
+    }
+
+    @Test
+    public void stringLiteral() {
+        Variable variable = smodelCreator.createVariable("variable", DataType.STRING,
+                smodelCreator.createStringLiteral("s"));
+
+        String actualValue = provider.getStringValue(variable);
+
+        assertThat(actualValue).isEqualTo("s");
     }
 }
