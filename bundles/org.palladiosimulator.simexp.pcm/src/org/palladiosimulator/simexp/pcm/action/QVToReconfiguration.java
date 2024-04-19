fba8423571e9cd0a9d8e33e1e3b508e6dd57913c
Ralf Sieger <sieger@fzi.de>
2024-04-19 08:57:05 +0200

    QVTO transformstion is now applied in QVToReconfiguration

--- bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/action/QVToReconfiguration.java
+++ bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/action/QVToReconfiguration.java
@@ -1,41 +1,65 @@
 package org.palladiosimulator.simexp.pcm.action;
 
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.ECollections;
 import org.palladiosimulator.simexp.core.action.Reconfiguration;
+import org.palladiosimulator.simexp.pcm.state.PcmArchitecturalConfiguration;
+import org.palladiosimulator.simexp.pcm.util.IExperimentProvider;
 import org.palladiosimulator.simulizar.reconfiguration.qvto.QVTOReconfigurator;
 import org.palladiosimulator.simulizar.reconfiguration.qvto.QvtoModelTransformation;
 
+import de.uka.ipd.sdq.scheduler.resources.active.IResourceTableManager;
+
 public class QVToReconfiguration extends Reconfiguration<QVTOReconfigurator> {
+    private static final Logger LOGGER = Logger.getLogger(PcmArchitecturalConfiguration.class);
+    private static final String EMPTY_RECONFIGURATION_NAME = "EmptyReconf";
 
-	private final static String EMPTY_RECONFIGURATION_NAME = "EmptyReconf";
+    private final QvtoModelTransformation transformation;
 
-	private final QvtoModelTransformation transformation;
+    protected QVToReconfiguration(QVToReconfiguration transformation) {
+        this(transformation.transformation);
+    }
 
-	protected QVToReconfiguration(QvtoModelTransformation transformation) {
-		this.transformation = transformation;
-	}
+    protected QVToReconfiguration(QvtoModelTransformation transformation) {
+        this.transformation = transformation;
+    }
 
-	public static QVToReconfiguration of(QvtoModelTransformation transformation) {
-		return new QVToReconfiguration(transformation);
-	}
+    public static QVToReconfiguration of(QvtoModelTransformation transformation) {
+        return new QVToReconfiguration(transformation);
+    }
 
-	public static QVToReconfiguration empty() {
-		return new QVToReconfiguration(null);
-	}
+    public static QVToReconfiguration empty() {
+        return new QVToReconfiguration((QvtoModelTransformation) null);
+    }
 
-	public QvtoModelTransformation getTransformation() {
-		return transformation;
-	}
+    public void apply(IExperimentProvider experimentProvider, IResourceTableManager resourceTableManager,
+            IQVToReconfigurationManager qvtoReconfigurationManager) {
+        if (isEmptyReconfiguration()) {
+            return;
+        }
+        QVTOReconfigurator qvtoReconf = qvtoReconfigurationManager.getReconfigurator(experimentProvider);
+        String transformationName = transformation.getTransformationName();
+        final boolean succeded = qvtoReconf.runExecute(ECollections.asEList(transformation), null,
+                resourceTableManager);
+        if (succeded) {
+            LOGGER.info(String.format("'EXECUTE' applied reconfiguration '%s'", transformationName));
+        } else {
+            LOGGER.error(String.format(
+                    "'EXECUTE' failed to apply reconfiguration: reconfiguration engine could not execute reconfiguration '%s'",
+                    transformationName));
+        }
+    }
 
-	public boolean isEmptyReconfiguration() {
-		return transformation == null;
-	}
+    private boolean isEmptyReconfiguration() {
+        return transformation == null;
+    }
 
-	@Override
-	public String getStringRepresentation() {
-		if (isEmptyReconfiguration()) {
-			return EMPTY_RECONFIGURATION_NAME;
-		}
-		return transformation.getTransformationName();
-	}
+    @Override
+    public String getStringRepresentation() {
+        if (isEmptyReconfiguration()) {
+            return EMPTY_RECONFIGURATION_NAME;
+        }
+        return transformation.getTransformationName();
+    }
 
 }
