b41bd9a6e389aea31c3e40f9f11ec10fa8be75d2
Ralf Sieger <sieger@fzi.de>
2024-04-19 09:41:20 +0200

    added support for multi QVTO transformations

---
+++ bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/action/MultiQVToReconfiguration.java
@@ -0,0 +1,64 @@
+package org.palladiosimulator.simexp.pcm.action;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.ECollections;
+import org.palladiosimulator.simexp.pcm.state.IPCMReconfigurationExecutor;
+import org.palladiosimulator.simexp.pcm.util.IExperimentProvider;
+import org.palladiosimulator.simulizar.reconfiguration.qvto.QVTOReconfigurator;
+import org.palladiosimulator.simulizar.reconfiguration.qvto.QvtoModelTransformation;
+
+import de.uka.ipd.sdq.scheduler.resources.active.IResourceTableManager;
+
+public class MultiQVToReconfiguration extends BaseQVToReconfiguration implements IPCMReconfigurationExecutor {
+    private static final Logger LOGGER = Logger.getLogger(MultiQVToReconfiguration.class);
+
+    private final List<QvtoModelTransformation> transformations;
+
+    protected MultiQVToReconfiguration(List<QvtoModelTransformation> transformations,
+            IQVToReconfigurationManager qvtoReconfigurationManager) {
+        super(qvtoReconfigurationManager);
+        this.transformations = Collections.unmodifiableList(transformations);
+    }
+
+    public static MultiQVToReconfiguration of(List<QvtoModelTransformation> transformations,
+            IQVToReconfigurationManager qvtoReconfigurationManager) {
+        return new MultiQVToReconfiguration(transformations, qvtoReconfigurationManager);
+    }
+
+    @Override
+    public void execute(IExperimentProvider experimentProvider, IResourceTableManager resourceTableManager) {
+        LOGGER.info(String.format("'EXECUTE' applying %d reconfigurations", transformations.size()));
+        QVTOReconfigurator qvtoReconf = qvtoReconfigurationManager.getReconfigurator(experimentProvider);
+        for (QvtoModelTransformation transformation : transformations) {
+            String transformationName = transformation.getTransformationName();
+            boolean succeded = qvtoReconf.runExecute(ECollections.asEList(transformation), null, resourceTableManager);
+            if (succeded) {
+                LOGGER.info(String.format("'EXECUTE' applied reconfiguration '%s'", transformationName));
+            } else {
+                LOGGER.error(String.format(
+                        "'EXECUTE' failed to apply reconfiguration: reconfiguration engine could not execute reconfiguration '%s'",
+                        transformationName));
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected boolean isEmptyReconfiguration() {
+        return transformations.isEmpty();
+    }
+
+    @Override
+    protected String getTransformationName() {
+        List<String> names = new ArrayList<>();
+        for (QvtoModelTransformation trafo : transformations) {
+            names.add(trafo.getTransformationName());
+        }
+        return StringUtils.join(names, ",");
+    }
+}
