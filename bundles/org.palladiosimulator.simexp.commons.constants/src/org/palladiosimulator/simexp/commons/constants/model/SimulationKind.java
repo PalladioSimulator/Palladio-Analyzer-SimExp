e984547418bc914e6d567436d3e1d7b5039a594c
Ralf Sieger <sieger@fzi.de>
2024-04-22 08:26:18 +0200

    removed superfluous valid objectives

--- bundles/org.palladiosimulator.simexp.commons.constants/src/org/palladiosimulator/simexp/commons/constants/model/SimulationKind.java
+++ bundles/org.palladiosimulator.simexp.commons.constants/src/org/palladiosimulator/simexp/commons/constants/model/SimulationKind.java
@@ -1,29 +1,19 @@
 package org.palladiosimulator.simexp.commons.constants.model;
 
-import java.util.Arrays;
-import java.util.List;
-
 public enum SimulationKind {
-    PERFORMANCE("Performance", Arrays.asList(QualityObjective.PERFORMANCE)), //
-    RELIABILITY("Reliability", Arrays.asList(QualityObjective.RELIABILITY)), //
-    PERFORMABILITY("Performability", Arrays.asList(QualityObjective.PERFORMABILITY)), //
-    MODELLED("Modelled",
-            Arrays.asList(QualityObjective.PERFORMANCE, QualityObjective.RELIABILITY, QualityObjective.PERFORMABILITY));
+    PERFORMANCE("Performance"), //
+    RELIABILITY("Reliability"), //
+    PERFORMABILITY("Performability"), //
+    MODELLED("Modelled");
 
     private final String name;
-    private final List<QualityObjective> validObjectives;
 
-    private SimulationKind(String name, List<QualityObjective> validObjectives) {
+    private SimulationKind(String name) {
         this.name = name;
-        this.validObjectives = validObjectives;
     }
 
     public String getName() {
         return name;
-    }
-
-    public List<QualityObjective> getValidObjectives() {
-        return validObjectives;
     }
 
     public static SimulationKind fromName(String name) {
