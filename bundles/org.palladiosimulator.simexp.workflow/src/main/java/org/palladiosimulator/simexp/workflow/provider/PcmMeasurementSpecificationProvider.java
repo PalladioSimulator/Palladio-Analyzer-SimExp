ef8feaa593c17730aa608708b5eff99f5f903b78
Martina Rapp <rapp@fzi.de>
2024-04-22 14:04:14 +0200

    improved log message

--- bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/provider/PcmMeasurementSpecificationProvider.java
+++ bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/provider/PcmMeasurementSpecificationProvider.java
@@ -15,36 +15,37 @@ import org.palladiosimulator.simexp.pcm.state.PcmMeasurementSpecification;
 import org.palladiosimulator.simexp.pcm.state.PcmMeasurementSpecification.MeasurementAggregator;
 
 public class PcmMeasurementSpecificationProvider {
-	private static final String RESPONSE_TIME_MONITOR = "System Response Time";
-	private static final String SYSTEM_EXECUTION_RESULTTYPE = "System ExecutionResultType";
+    private static final String RESPONSE_TIME_MONITOR = "System Response Time";
+    private static final String SYSTEM_EXECUTION_RESULTTYPE = "System ExecutionResultType";
 
-	private final Experiment experiment;
+    private final Experiment experiment;
 
-	public PcmMeasurementSpecificationProvider(Experiment experiment) {
-		this.experiment = experiment;
-	}
+    public PcmMeasurementSpecificationProvider(Experiment experiment) {
+        this.experiment = experiment;
+    }
 
-	public PcmMeasurementSpecification getSpecification(String monitorName) {
-		return switch (monitorName) {
-			case RESPONSE_TIME_MONITOR -> buildResponseTimeSpec();
-			case SYSTEM_EXECUTION_RESULTTYPE -> buildSystemExecutionResultTypeSpec();
-			default -> buildCpuUtilizationSpecOf(monitorName);
-		};
-	}
+    public PcmMeasurementSpecification getSpecification(String monitorName) {
+        return switch (monitorName) {
+        case RESPONSE_TIME_MONITOR -> buildResponseTimeSpec();
+        case SYSTEM_EXECUTION_RESULTTYPE -> buildSystemExecutionResultTypeSpec();
+        default -> buildCpuUtilizationSpecOf(monitorName);
+        };
+    }
 
-	private PcmMeasurementSpecification buildResponseTimeSpec() {
-		Monitor rtMonitor = findMonitor(RESPONSE_TIME_MONITOR);
-		MeasurementSpecification rtSpec = rtMonitor.getMeasurementSpecifications().get(0);
-		return PcmMeasurementSpecification.newBuilder()
-				.withName(rtMonitor.getEntityName())
-				.measuredAt(rtMonitor.getMeasuringPoint())
-				.withMetric(rtSpec.getMetricDescription())
-				.useDefaultMeasurementAggregation()
-				.withOptionalSteadyStateEvaluator(Threshold.lessThan(0.1))
-				.build();
-	}
+    private PcmMeasurementSpecification buildResponseTimeSpec() {
+        Monitor rtMonitor = findMonitor(RESPONSE_TIME_MONITOR);
+        MeasurementSpecification rtSpec = rtMonitor.getMeasurementSpecifications()
+            .get(0);
+        return PcmMeasurementSpecification.newBuilder()
+            .withName(rtMonitor.getEntityName())
+            .measuredAt(rtMonitor.getMeasuringPoint())
+            .withMetric(rtSpec.getMetricDescription())
+            .useDefaultMeasurementAggregation()
+            .withOptionalSteadyStateEvaluator(Threshold.lessThan(0.1))
+            .build();
+    }
 
-	private PcmMeasurementSpecification buildSystemExecutionResultTypeSpec() {
+    private PcmMeasurementSpecification buildSystemExecutionResultTypeSpec() {
         Monitor monitor = findMonitor(SYSTEM_EXECUTION_RESULTTYPE);
         EList<MeasurementSpecification> measurementSpecifications = monitor.getMeasurementSpecifications();
         // this is a MetricDescriptionSet -> thus you need to add the contained BaseMetric
@@ -61,22 +62,27 @@ public class PcmMeasurementSpecificationProvider {
             .build();
     }
 
-	private PcmMeasurementSpecification buildCpuUtilizationSpecOf(String monitorName) {
-		Monitor monitor = findMonitor(monitorName);
-		MeasurementSpecification spec = monitor.getMeasurementSpecifications().get(1);
-		MeasurementAggregator utilizationAggregator = new UtilizationAggregator();
+    private PcmMeasurementSpecification buildCpuUtilizationSpecOf(String monitorName) {
+        Monitor monitor = findMonitor(monitorName);
+        MeasurementSpecification spec = monitor.getMeasurementSpecifications()
+            .get(1);
+        MeasurementAggregator utilizationAggregator = new UtilizationAggregator();
         return PcmMeasurementSpecification.newBuilder()
-				.withName(monitor.getEntityName())
-				.measuredAt(monitor.getMeasuringPoint())
-				.withMetric(spec.getMetricDescription())
-				.aggregateMeasurementsBy(utilizationAggregator)
-				.build();
-	}
+            .withName(monitor.getEntityName())
+            .measuredAt(monitor.getMeasuringPoint())
+            .withMetric(spec.getMetricDescription())
+            .aggregateMeasurementsBy(utilizationAggregator)
+            .build();
+    }
 
-	private Monitor findMonitor(String monitorName) {
-		Stream<Monitor> monitors = experiment.getInitialModel().getMonitorRepository().getMonitors().stream();
-		return monitors.filter(m -> m.getEntityName().equals(monitorName))
-					   .findFirst()
-					   .orElseThrow(() -> new RuntimeException("There is no monitor."));
-	}
+    private Monitor findMonitor(String monitorName) {
+        Stream<Monitor> monitors = experiment.getInitialModel()
+            .getMonitorRepository()
+            .getMonitors()
+            .stream();
+        return monitors.filter(m -> m.getEntityName()
+            .equals(monitorName))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("There is no monitor '" + monitorName + "' ."));
+    }
 }
