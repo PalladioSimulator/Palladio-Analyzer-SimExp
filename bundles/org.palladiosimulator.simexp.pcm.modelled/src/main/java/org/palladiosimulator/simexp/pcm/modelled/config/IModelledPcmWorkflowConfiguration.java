0cfe4d83099125758611a267d662ed0c28adf6ee
Ralf Sieger <sieger@fzi.de>
2024-04-25 14:12:32 +0200

    introduced specialized modelled workflow interfaces

---
+++ bundles/org.palladiosimulator.simexp.pcm.modelled/src/main/java/org/palladiosimulator/simexp/pcm/modelled/config/IModelledPcmWorkflowConfiguration.java
@@ -0,0 +1,7 @@
+package org.palladiosimulator.simexp.pcm.modelled.config;
+
+import org.palladiosimulator.simexp.pcm.config.IPCMConfiguration;
+
+public interface IModelledPcmWorkflowConfiguration extends IModelledWorkflowConfiguration, IPCMConfiguration {
+
+}
---
+++ bundles/org.palladiosimulator.simexp.pcm.modelled/src/main/java/org/palladiosimulator/simexp/pcm/modelled/config/IModelledPrismWorkflowConfiguration.java
@@ -0,0 +1,7 @@
+package org.palladiosimulator.simexp.pcm.modelled.config;
+
+import org.palladiosimulator.simexp.pcm.config.IPrismConfiguration;
+
+public interface IModelledPrismWorkflowConfiguration extends IModelledWorkflowConfiguration, IPrismConfiguration {
+
+}
