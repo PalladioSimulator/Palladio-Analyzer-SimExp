7401c612754a999a19b93cb69e26dcdc047f094f
Ralf Sieger <sieger@fzi.de>
2024-04-25 16:37:28 +0200

    introduced SimulatorPcmExperienceSimulationExecutorFactory

---
+++ bundles/org.palladiosimulator.simexp.pcm.simulator/src/main/java/org/palladiosimulator/simexp/pcm/simulator/SimulatorPcmExperienceSimulationExecutorFactory.java
@@ -0,0 +1,32 @@
+package org.palladiosimulator.simexp.pcm.simulator;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.palladiosimulator.envdyn.api.entity.bn.DynamicBayesianNetwork;
+import org.palladiosimulator.experimentautomation.experiments.Experiment;
+import org.palladiosimulator.simexp.core.entity.SimulatedMeasurementSpecification;
+import org.palladiosimulator.simexp.core.store.SimulatedExperienceStore;
+import org.palladiosimulator.simexp.pcm.config.IPCMWorkflowConfiguration;
+import org.palladiosimulator.simexp.pcm.config.SimulationParameters;
+import org.palladiosimulator.simexp.pcm.examples.executor.PcmExperienceSimulationExecutorFactory;
+import org.palladiosimulator.simulizar.reconfiguration.qvto.QVTOReconfigurator;
+
+import tools.mdsd.probdist.api.apache.util.IProbabilityDistributionRepositoryLookup;
+import tools.mdsd.probdist.api.entity.CategoricalValue;
+import tools.mdsd.probdist.api.factory.IProbabilityDistributionFactory;
+import tools.mdsd.probdist.api.factory.IProbabilityDistributionRegistry;
+import tools.mdsd.probdist.api.parser.ParameterParser;
+
+public abstract class SimulatorPcmExperienceSimulationExecutorFactory<R extends Number, V, T extends SimulatedMeasurementSpecification>
+        extends PcmExperienceSimulationExecutorFactory<R, V, T> {
+    public SimulatorPcmExperienceSimulationExecutorFactory(IPCMWorkflowConfiguration workflowConfiguration,
+            ResourceSet rs, Experiment experiment, DynamicBayesianNetwork<CategoricalValue> dbn, List<T> specs,
+            SimulationParameters params, SimulatedExperienceStore<QVTOReconfigurator, R> simulatedExperienceStore,
+            IProbabilityDistributionFactory<CategoricalValue> distributionFactory,
+            IProbabilityDistributionRegistry<CategoricalValue> probabilityDistributionRegistry,
+            ParameterParser parameterParser, IProbabilityDistributionRepositoryLookup probDistRepoLookup) {
+        super(workflowConfiguration, rs, experiment, dbn, specs, params, simulatedExperienceStore, distributionFactory,
+                probabilityDistributionRegistry, parameterParser, probDistRepoLookup);
+    }
+}
