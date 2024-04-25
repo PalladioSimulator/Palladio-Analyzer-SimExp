ba7e4857ef902e5c546281aeae3d55c936a09122
Ralf Sieger <sieger@fzi.de>
2024-04-25 11:19:07 +0200

    made attributes final

--- bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/config/SimulationParameters.java
+++ bundles/org.palladiosimulator.simexp.pcm/src/org/palladiosimulator/simexp/pcm/config/SimulationParameters.java
@@ -1,25 +1,25 @@
 package org.palladiosimulator.simexp.pcm.config;
 
 public class SimulationParameters {
-	private String simulationID;
-	private int numberOfRuns;
-	private int numberOfSimulationsPerRun;
-	
-	public SimulationParameters(String simulationId, int numberOfRuns, int numberOfSimulationsPerRun) {
-		this.simulationID = simulationId;
-		this.numberOfRuns = numberOfRuns;
-		this.numberOfSimulationsPerRun = numberOfSimulationsPerRun;
-	}
-	
-	public String getSimulationID() {
-		return simulationID;
-	}
-	
-	public int getNumberOfRuns() {
-		return numberOfRuns;
-	}
-	
-	public int getNumberOfSimulationsPerRun() {
-		return numberOfSimulationsPerRun;
-	}
+    private final String simulationID;
+    private final int numberOfRuns;
+    private final int numberOfSimulationsPerRun;
+
+    public SimulationParameters(String simulationId, int numberOfRuns, int numberOfSimulationsPerRun) {
+        this.simulationID = simulationId;
+        this.numberOfRuns = numberOfRuns;
+        this.numberOfSimulationsPerRun = numberOfSimulationsPerRun;
+    }
+
+    public String getSimulationID() {
+        return simulationID;
+    }
+
+    public int getNumberOfRuns() {
+        return numberOfRuns;
+    }
+
+    public int getNumberOfSimulationsPerRun() {
+        return numberOfSimulationsPerRun;
+    }
 }
