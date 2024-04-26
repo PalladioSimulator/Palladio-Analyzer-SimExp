bfeec12e2455749eb68898d664d45abf567c5a61
Ralf Sieger <sieger@fzi.de>
2024-04-26 16:01:24 +0200

    reverted last change

--- bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/config/SimExpWorkflowConfiguration.java
+++ bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/config/SimExpWorkflowConfiguration.java
@@ -51,12 +51,7 @@ public class SimExpWorkflowConfiguration extends AbstractPCMWorkflowRunConfigura
         this.setUsageModelFile(architecturalModels.getUsageModelFile());
         this.setAllocationFiles(architecturalModels.getAllocationFiles());
         this.experimentsFile = URI.createURI(architecturalModels.getExperimentsFile());
-        String smodelFileUriStr = architecturalModels.getSmodelFile();
-        if (smodelFileUriStr != null) {
-            this.smodelFile = URI.createURI(smodelFileUriStr);
-        } else {
-            this.smodelFile = null;
-        }
+        this.smodelFile = URI.createURI(architecturalModels.getSmodelFile());
         this.staticModelFile = URI.createURI(environmentalModels.getStaticModelFile());
         this.dynamicModelFile = URI.createURI(environmentalModels.getDynamicModelFile());
         this.monitorRepositoryFile = URI.createURI(monitors.getMonitorRepositoryFile());
