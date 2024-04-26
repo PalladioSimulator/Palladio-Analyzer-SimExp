fc9473f3784aab53f7e92fcd859bb78ebc125f2c
Ralf Sieger <sieger@fzi.de>
2024-04-26 12:53:09 +0200

    introduced ModelledExperienceSimulationExecutorFactory

--- bundles/org.palladiosimulator.simexp.pcm.modelled/src/main/java/org/palladiosimulator/simexp/pcm/modelled/ModelledExperienceSimulationExecutorFactory.java
+++ bundles/org.palladiosimulator.simexp.pcm.modelled/src/main/java/org/palladiosimulator/simexp/pcm/modelled/ModelledExperienceSimulationExecutorFactory.java
@@ -1,13 +1,16 @@
 package org.palladiosimulator.simexp.pcm.modelled;
 
-import java.util.List;
-
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.palladiosimulator.envdyn.api.entity.bn.DynamicBayesianNetwork;
 import org.palladiosimulator.experimentautomation.experiments.Experiment;
 import org.palladiosimulator.simexp.core.entity.SimulatedMeasurementSpecification;
 import org.palladiosimulator.simexp.core.store.SimulatedExperienceStore;
+import org.palladiosimulator.simexp.dsl.smodel.smodel.Smodel;
+import org.palladiosimulator.simexp.model.io.SModelLoader;
 import org.palladiosimulator.simexp.pcm.config.SimulationParameters;
+import org.palladiosimulator.simexp.pcm.examples.executor.PcmExperienceSimulationExecutorFactory;
 import org.palladiosimulator.simexp.pcm.modelled.config.IModelledWorkflowConfiguration;
 import org.palladiosimulator.simulizar.reconfiguration.qvto.QVTOReconfigurator;
 
@@ -17,24 +20,28 @@ import tools.mdsd.probdist.api.factory.IProbabilityDistributionFactory;
 import tools.mdsd.probdist.api.factory.IProbabilityDistributionRegistry;
 import tools.mdsd.probdist.api.parser.ParameterParser;
 
-public abstract class ModelledPcmExperienceSimulationExecutorFactory<R extends Number, V, T extends SimulatedMeasurementSpecification>
-        extends ModelledExperienceSimulationExecutorFactory<R, V, T> {
+public abstract class ModelledExperienceSimulationExecutorFactory<R extends Number, V, T extends SimulatedMeasurementSpecification>
+        extends PcmExperienceSimulationExecutorFactory<R, V, T> {
+    private static final Logger LOGGER = Logger.getLogger(ModelledExperienceSimulationExecutorFactory.class);
 
-    private final List<T> specs;
+    private final Smodel smodel;
 
-    public ModelledPcmExperienceSimulationExecutorFactory(IModelledWorkflowConfiguration workflowConfiguration,
-            ResourceSet rs, Experiment experiment, DynamicBayesianNetwork<CategoricalValue> dbn, List<T> specs,
+    public ModelledExperienceSimulationExecutorFactory(IModelledWorkflowConfiguration workflowConfiguration,
+            ResourceSet rs, Experiment experiment, DynamicBayesianNetwork<CategoricalValue> dbn,
             SimulationParameters params, SimulatedExperienceStore<QVTOReconfigurator, R> simulatedExperienceStore,
             IProbabilityDistributionFactory<CategoricalValue> distributionFactory,
             IProbabilityDistributionRegistry<CategoricalValue> probabilityDistributionRegistry,
             ParameterParser parameterParser, IProbabilityDistributionRepositoryLookup probDistRepoLookup) {
         super(workflowConfiguration, rs, experiment, dbn, params, simulatedExperienceStore, distributionFactory,
                 probabilityDistributionRegistry, parameterParser, probDistRepoLookup);
-        this.specs = specs;
+        URI smodelURI = workflowConfiguration.getSmodelURI();
+        SModelLoader smodelLoader = new SModelLoader();
+        this.smodel = smodelLoader.load(rs, smodelURI);
+        LOGGER.debug(String.format("Loaded smodel from '%s'", smodelURI.path()));
     }
 
-    @Override
-    protected List<T> createSpecs() {
-        return specs;
+    protected Smodel getSmodel() {
+        return smodel;
     }
+
 }
