422b01de9f267ef80e37cda3bec5f5158c5cf7f7
Ralf Sieger <sieger@fzi.de>
2024-11-15 13:49:37 +0100

    added logging support

--- bundles/org.palladiosimulator.simexp.dsl.ea.optimizer/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/impl/EAOptimizer.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.optimizer/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/impl/EAOptimizer.java
@@ -1,15 +1,18 @@
 package org.palladiosimulator.simexp.dsl.ea.optimizer.impl;
 
+import org.apache.log4j.Logger;
 import org.palladiosimulator.simexp.dsl.ea.api.IEAEvolutionStatusReceiver;
 import org.palladiosimulator.simexp.dsl.ea.api.IEAFitnessEvaluator;
 import org.palladiosimulator.simexp.dsl.ea.api.IEAOptimizer;
 import org.palladiosimulator.simexp.dsl.ea.api.IOptimizableProvider;
 
 public class EAOptimizer implements IEAOptimizer {
+    private final static Logger LOGGER = Logger.getLogger(EAOptimizer.class);
 
     @Override
     public void optimize(IOptimizableProvider optimizableProvider, IEAFitnessEvaluator fitnessEvaluator,
             IEAEvolutionStatusReceiver evolutionStatusReceiver) {
+        LOGGER.info("EA running...");
         // TODO Auto-generated method stub
 
     }
