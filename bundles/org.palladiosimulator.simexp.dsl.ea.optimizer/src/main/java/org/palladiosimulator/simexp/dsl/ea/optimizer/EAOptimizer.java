aeb4a297b513a1f95cba31ed319b63566075f3ca
Ralf Sieger <sieger@fzi.de>
2024-11-15 13:33:56 +0100

    fixed build issues

---
+++ bundles/org.palladiosimulator.simexp.dsl.ea.optimizer/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/EAOptimizer.java
@@ -0,0 +1,15 @@
+package org.palladiosimulator.simexp.dsl.ea.optimizer;
+
+import org.palladiosimulator.simexp.dsl.ea.api.IFitnessEvaluator;
+import org.palladiosimulator.simexp.dsl.ea.api.IOptimizableProvider;
+import org.palladiosimulator.simexp.dsl.ea.api.IOptimizer;
+
+public class EAOptimizer implements IOptimizer {
+
+    @Override
+    public void optimize(IOptimizableProvider optimizableProvider, IFitnessEvaluator fitnessEvaluator) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
