5e0841ff18bce6f429feea0d995c12d8766dcab7
Ralf Sieger <sieger@fzi.de>
2024-11-15 13:36:32 +0100

    added concrete test class

---
+++ tests/org.palladiosimulator.simexp.dsl.ea.optimizer.tests/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/impl/EAOptimizerTest.java
@@ -0,0 +1,12 @@
+package org.palladiosimulator.simexp.dsl.ea.optimizer.impl;
+
+import org.junit.Test;
+
+public class EAOptimizerTest {
+
+    @Test
+    public void removeMe() {
+        // TODO
+    }
+
+}
