a08237b29adf7bc88d658ae786000462d55b238c
Ralf Sieger <sieger@fzi.de>
2024-11-15 13:38:01 +0100

    added basic configuration interface

--- bundles/org.palladiosimulator.simexp.dsl.ea.optimizer/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/EAOptimizerFactory.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.optimizer/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/EAOptimizerFactory.java
@@ -1,10 +1,11 @@
 package org.palladiosimulator.simexp.dsl.ea.optimizer;
 
+import org.palladiosimulator.simexp.dsl.ea.api.IEAConfig;
 import org.palladiosimulator.simexp.dsl.ea.api.IEAOptimizer;
 import org.palladiosimulator.simexp.dsl.ea.optimizer.impl.EAOptimizer;
 
 public class EAOptimizerFactory {
-    public IEAOptimizer create() {
+    public IEAOptimizer create(IEAConfig eaConfig) {
         return new EAOptimizer();
     }
 }
