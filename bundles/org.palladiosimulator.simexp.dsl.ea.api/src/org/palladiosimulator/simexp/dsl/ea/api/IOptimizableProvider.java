2a2f037f22307a9ed57fe3c34b7ddc96d3204586
Ralf Sieger <sieger@fzi.de>
2024-11-21 11:07:47 +0100

    added support to evaluate expresssions in the optimizer

--- bundles/org.palladiosimulator.simexp.dsl.ea.api/src/org/palladiosimulator/simexp/dsl/ea/api/IOptimizableProvider.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.api/src/org/palladiosimulator/simexp/dsl/ea/api/IOptimizableProvider.java
@@ -2,8 +2,11 @@ package org.palladiosimulator.simexp.dsl.ea.api;
 
 import java.util.Collection;
 
+import org.palladiosimulator.simexp.dsl.smodel.api.IExpressionCalculator;
 import org.palladiosimulator.simexp.dsl.smodel.smodel.Optimizable;
 
 public interface IOptimizableProvider {
     Collection<Optimizable> getOptimizables();
+
+    IExpressionCalculator getExpressionCalculator();
 }
