761b4c13341314dc007d0ff2ee4a9b44f0931586
Ralf Sieger <sieger@fzi.de>
2024-11-25 13:47:51 +0100

    added log initialization and basic startup

--- bundles/org.palladiosimulator.simexp.app.console/src/main/java/org/palladiosimulator/simexp/app/console/SimExpApplication.java
+++ bundles/org.palladiosimulator.simexp.app.console/src/main/java/org/palladiosimulator/simexp/app/console/SimExpApplication.java
@@ -1,20 +1,76 @@
 package org.palladiosimulator.simexp.app.console;
 
+import java.io.IOException;
+import java.lang.Thread.UncaughtExceptionHandler;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.osgi.service.datalocation.Location;
 
 public class SimExpApplication implements IApplication {
+    private Logger logger;
 
     @Override
     public Object start(IApplicationContext context) throws Exception {
-        // TODO Auto-generated method stub
-        return null;
+        initLogging();
+        logger = Logger.getLogger(SimExpApplication.class);
+
+        logger.info("SimExp running...");
+
+        init();
+        if (!lockInstanceLocation()) {
+            return IApplication.EXIT_OK;
+        }
+
+        // TODO:
+
+        return IApplication.EXIT_OK;
+    }
+
+    private void initLogging() {
+        PatternLayout consoleLayout = new PatternLayout("%d %-5p [%-10t] [%F:%L]: %m%n");
+        ConsoleAppender ca = new ConsoleAppender(consoleLayout);
+        ca.setThreshold(Level.DEBUG);
+        BasicConfigurator.resetConfiguration();
+        BasicConfigurator.configure(ca);
+    }
+
+    private void init() {
+        registerTerminationHandlers();
+    }
+
+    private void registerTerminationHandlers() {
+        Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+
+            @Override
+            public void uncaughtException(Thread arg0, Throwable arg1) {
+                logger.error("Uncaught exception in thread: " + arg0, arg1);
+            }
+        });
+    }
+
+    private boolean lockInstanceLocation() throws IOException {
+        Location instanceLocation = Platform.getConfigurationLocation();
+        String instancePath = instanceLocation.getURL()
+            .getPath();
+        logger.info(String.format("instance path: %s", instancePath));
+        if (!instanceLocation.lock()) {
+            logger.error("application is already running");
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void stop() {
+        logger.info("stopped");
         // TODO Auto-generated method stub
 
     }
-
 }
