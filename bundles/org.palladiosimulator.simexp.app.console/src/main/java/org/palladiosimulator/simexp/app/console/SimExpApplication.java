c712acf9df45ff6cee2a6bc2d3b726f12b5db4fb
Ralf Sieger <sieger@fzi.de>
2024-11-25 12:30:41 +0100

    initial simexp application working

---
+++ bundles/org.palladiosimulator.simexp.app.console/src/main/java/org/palladiosimulator/simexp/app/console/SimExpApplication.java
@@ -0,0 +1,20 @@
+package org.palladiosimulator.simexp.app.console;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class SimExpApplication implements IApplication {
+
+    @Override
+    public Object start(IApplicationContext context) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
