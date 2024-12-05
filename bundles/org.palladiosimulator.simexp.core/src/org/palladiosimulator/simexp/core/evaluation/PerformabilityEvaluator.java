4bf18ee9766246aad0d6d610c873916d94648c3f
Martina Rapp <rapp@fzi.de>
2024-12-05 12:37:43 +0100

    introduced name to distinguish between various types of reward calculation

--- bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/evaluation/PerformabilityEvaluator.java
+++ bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/evaluation/PerformabilityEvaluator.java
@@ -8,54 +8,61 @@ import org.apache.log4j.Logger;
 import org.palladiosimulator.simexp.core.entity.SimulatedExperience;
 
 public class PerformabilityEvaluator implements TotalRewardCalculation {
-    
+
     private static final Logger LOGGER = Logger.getLogger(PerformabilityEvaluator.class.getName());
-    
+
     private final String simulationId;
     private final String sampleSpaceId;
-    
+
     public PerformabilityEvaluator(String simulationId, String sampleSpaceId) {
         this.simulationId = simulationId;
         this.sampleSpaceId = sampleSpaceId;
     }
 
-    
     public static TotalRewardCalculation of(String simulationId, String sampleSpaceId) {
         return new PerformabilityEvaluator(simulationId, sampleSpaceId);
     }
-    
+
     @Override
     public double computeTotalReward() {
         double totalReward = 0;
-        
+
         List<Double> responseTimes = new ArrayList<>();
         SampleModelIterator iterator = SampleModelIterator.get(simulationId, sampleSpaceId);
-        
+
         /**
-         * total reward computation:
-         * aggregate data based on the performability metric, i.e. calculate the expected value of all measured response times.
+         * total reward computation: aggregate data based on the performability metric, i.e.
+         * calculate the expected value of all measured response times.
          * 
-         * */
+         */
         while (iterator.hasNext()) {
             for (SimulatedExperience exp : iterator.next()) {
                 double responseTime = retrieveResponseTime(exp);
                 responseTimes.add(responseTime);
-                }
+            }
         }
-        
+
         if (!responseTimes.isEmpty()) {
-            double addedUpResponseTimes = responseTimes.stream().mapToDouble(Double::doubleValue).sum();
-            totalReward =  addedUpResponseTimes / responseTimes.size();
+            double addedUpResponseTimes = responseTimes.stream()
+                .mapToDouble(Double::doubleValue)
+                .sum();
+            totalReward = addedUpResponseTimes / responseTimes.size();
         }
-        
-        LOGGER.debug(String.format(Locale.ENGLISH, "Computed performability reward: expectation(response time): %.5f", totalReward));
+
+        LOGGER.debug(String.format(Locale.ENGLISH, "Computed performability reward: expectation(response time): %.5f",
+                totalReward));
         return totalReward;
     }
-    
+
     private double retrieveResponseTime(SimulatedExperience exp) {
         // response time reward format is of type double
         String reward = exp.getReward();
         return Double.parseDouble(reward);
+    }
+
+    @Override
+    public String getName() {
+        return "performability accumulated";
     }
 
 }
--- bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/evaluation/TotalRewardCalculation.java
+++ bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/evaluation/TotalRewardCalculation.java
@@ -2,6 +2,8 @@ package org.palladiosimulator.simexp.core.evaluation;
 
 public interface TotalRewardCalculation {
 
+    String getName();
+
     double computeTotalReward();
 
 }
