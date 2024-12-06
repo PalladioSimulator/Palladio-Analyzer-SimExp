6e489285521e1e4f21585c46e280cfbfc6bdbc15
Ralf Sieger <sieger@fzi.de>
2024-12-06 09:06:20 +0100

    extracted SpecialCaseStateQuantity into separate file

--- bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/state/RestoredSelfAdaptiveSystemState.java
+++ bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/state/RestoredSelfAdaptiveSystemState.java
@@ -1,56 +1,9 @@
 package org.palladiosimulator.simexp.core.state;
 
-import java.util.Optional;
-import java.util.stream.Stream;
-
 import org.palladiosimulator.simexp.core.entity.SimulatedExperience;
-import org.palladiosimulator.simexp.core.entity.SimulatedMeasurement;
-import org.palladiosimulator.simexp.core.entity.SimulatedMeasurementSpecification;
 import org.palladiosimulator.simexp.environmentaldynamics.entity.PerceivableEnvironmentalState;
 
 public class RestoredSelfAdaptiveSystemState<C, A, V> extends SelfAdaptiveSystemState<C, A, V> {
-
-    // TODO this is not a good solution: The class should be refactored in further iterations.
-    private static class SpecialCaseStateQuantity extends StateQuantity {
-
-        private final String quantifiedState;
-
-        public SpecialCaseStateQuantity(String quantifiedState) {
-            super(null);
-            this.quantifiedState = quantifiedState;
-        }
-
-        @Override
-        public Optional<SimulatedMeasurement> findMeasurementWith(SimulatedMeasurementSpecification spec) {
-            Stream<String> quantities = Stream.of(quantifiedState.split(withDelimiter()));
-            return quantities.filter(quantity -> quantity.contains(spec.getId()))
-                .map(quantity -> restoreSimulatedMeasurement(quantity, spec))
-                .findFirst();
-        }
-
-        private String withDelimiter() {
-            return "\\" + DELIMITER;
-        }
-
-        private SimulatedMeasurement restoreSimulatedMeasurement(String quantity,
-                SimulatedMeasurementSpecification spec) {
-            Double restoredValue = restoreValue(quantity);
-            SimulatedMeasurement measurement = SimulatedMeasurement.of(restoredValue, spec);
-            return measurement;
-        }
-
-        private Double restoreValue(String quantity) {
-            String value = quantity.split(" ")[1];
-            value = value.replace(",", "");
-            return Double.parseDouble(value);
-        }
-
-        @Override
-        public String toString() {
-            return quantifiedState;
-        }
-
-    }
 
     private final SpecialCaseStateQuantity quantifiedState;
     private final SelfAdaptiveSystemState<C, A, V> restoredState;
---
+++ bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/state/SpecialCaseStateQuantity.java
@@ -0,0 +1,50 @@
+package org.palladiosimulator.simexp.core.state;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.palladiosimulator.simexp.core.entity.SimulatedMeasurement;
+import org.palladiosimulator.simexp.core.entity.SimulatedMeasurementSpecification;
+
+/**
+ * TODO this is not a good solution: The class should be refactored in further iterations.
+ */
+public class SpecialCaseStateQuantity extends StateQuantity {
+
+    private final String quantifiedState;
+
+    public SpecialCaseStateQuantity(String quantifiedState) {
+        super(null);
+        this.quantifiedState = quantifiedState;
+    }
+
+    @Override
+    public Optional<SimulatedMeasurement> findMeasurementWith(SimulatedMeasurementSpecification spec) {
+        Stream<String> quantities = Stream.of(quantifiedState.split(withDelimiter()));
+        return quantities.filter(quantity -> quantity.contains(spec.getId()))
+            .map(quantity -> restoreSimulatedMeasurement(quantity, spec))
+            .findFirst();
+    }
+
+    private String withDelimiter() {
+        return "\\" + DELIMITER;
+    }
+
+    private SimulatedMeasurement restoreSimulatedMeasurement(String quantity, SimulatedMeasurementSpecification spec) {
+        Double restoredValue = restoreValue(quantity);
+        SimulatedMeasurement measurement = SimulatedMeasurement.of(restoredValue, spec);
+        return measurement;
+    }
+
+    private Double restoreValue(String quantity) {
+        String value = quantity.split(" ")[1];
+        value = value.replace(",", "");
+        return Double.parseDouble(value);
+    }
+
+    @Override
+    public String toString() {
+        return quantifiedState;
+    }
+
+}
