cfac51590730898fd306f885843ceac6f845f0aa
Ralf Sieger <sieger@fzi.de>
2024-12-11 09:53:00 +0100

    added kubernetes preference constants

---
+++ bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/preferences/KubernetesSettings.java
@@ -0,0 +1,6 @@
+package org.palladiosimulator.simexp.workflow.preferences;
+
+public interface KubernetesSettings {
+    static String CLUSTER_URL = "cluster url";
+    static String API_TOKEN = "api token";
+}
