bd3c7882f22387dc376c809bf29370a7ca1b587f
Martina Rapp <rapp@fzi.de>
2024-12-11 11:45:42 +0100

    added test class for Representation

---
+++ tests/org.palladiosimulator.simexp.dsl.ea.optimizer.tests/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/impl/tests/OptimizableRepresentationTest.java
@@ -0,0 +1,15 @@
+package org.palladiosimulator.simexp.dsl.ea.optimizer.impl.tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class OptimizableRepresentationTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+        // TODO: add test cases for each Optimizable type
+    }
+
+}
