78913a122dc5b7e71199c107761ea3273ef476c7
Martina Rapp <rapp@fzi.de>
2024-12-12 13:30:02 +0100

    constant value now references non-ui workflow plugin

--- bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/preferences/KubernetesSettings.java
+++ bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/preferences/KubernetesSettings.java
@@ -1,7 +1,7 @@
 package org.palladiosimulator.simexp.workflow.preferences;
 
 public interface KubernetesSettings {
-    static final String KUBERNETES_PREFERENCES = "org.palladiosimulator.simexp.ui.workflow.preferences";
+    static final String KUBERNETES_PREFERENCES = "org.palladiosimulator.simexp.workflow";
 
     static final String CLUSTER_URL = "cluster url";
     static final String API_TOKEN = "api token";
