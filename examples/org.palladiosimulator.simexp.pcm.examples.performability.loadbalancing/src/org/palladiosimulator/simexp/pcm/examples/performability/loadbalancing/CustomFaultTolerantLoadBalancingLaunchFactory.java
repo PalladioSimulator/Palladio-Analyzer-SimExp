8772343496f78da518422994a46f1710f07c8728
Martina Rapp <rapp@fzi.de>
2024-12-30 14:42:47 +0100

    renamed to custom factory

--- examples/org.palladiosimulator.simexp.pcm.examples.performability.loadbalancing/src/org/palladiosimulator/simexp/pcm/examples/performability/loadbalancing/CustomFaultTolerantLoadBalancingLaunchFactory.java
+++ examples/org.palladiosimulator.simexp.pcm.examples.performability.loadbalancing/src/org/palladiosimulator/simexp/pcm/examples/performability/loadbalancing/CustomFaultTolerantLoadBalancingLaunchFactory.java
@@ -15,7 +15,7 @@ import org.palladiosimulator.simexp.workflow.api.LaunchDescriptionProvider;
 
 import tools.mdsd.probdist.api.random.ISeedProvider;
 
-public class FaultTolerantLoadBalancingLaunchFactory implements ILaunchFactory {
+public class CustomFaultTolerantLoadBalancingLaunchFactory implements ILaunchFactory {
 
     @Override
     public boolean canHandle(IWorkflowConfiguration config) {
