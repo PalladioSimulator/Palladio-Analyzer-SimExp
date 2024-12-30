2406e157b0539f7efdac0e04cd1e86fd2edfa1de
Martina Rapp <rapp@fzi.de>
2024-12-30 14:35:22 +0100

    refactoring of reliability launch factory

--- bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/launcher/PcmSimulationExecutorFactory.java
+++ bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/launcher/PcmSimulationExecutorFactory.java
@@ -4,11 +4,7 @@ import java.util.Optional;
 
 import org.palladiosimulator.core.simulation.SimulationExecutor;
 import org.palladiosimulator.simexp.commons.constants.model.QualityObjective;
-import org.palladiosimulator.simexp.core.entity.SimulatedMeasurementSpecification;
 import org.palladiosimulator.simexp.core.store.DescriptionProvider;
-import org.palladiosimulator.simexp.core.store.SimulatedExperienceStore;
-import org.palladiosimulator.simexp.pcm.examples.executor.PcmExperienceSimulationExecutorFactory;
-import org.palladiosimulator.simexp.pcm.examples.hri.RobotCognitionSimulationExecutorFactory;
 import org.palladiosimulator.simexp.pcm.simulator.config.IPCMWorkflowConfiguration;
 
 import tools.mdsd.probdist.api.random.ISeedProvider;
@@ -19,14 +15,14 @@ public class PcmSimulationExecutorFactory extends BaseSimulationExecutorFactory 
             DescriptionProvider descriptionProvider, Optional<ISeedProvider> seedProvider) {
         PcmModelLoader.Factory modelLoaderFactory = new PcmModelLoader.Factory();
         QualityObjective qualityObjective = workflowConfiguration.getQualityObjective();
-        PcmExperienceSimulationExecutorFactory<? extends Number, ?, ? extends SimulatedMeasurementSpecification> factory = switch (qualityObjective) {
+//        PcmExperienceSimulationExecutorFactory<? extends Number, ?, ? extends SimulatedMeasurementSpecification> factory = switch (qualityObjective) {
         /*
          * case PERFORMANCE -> new LoadBalancingSimulationExecutorFactory(workflowConfiguration,
          * modelLoaderFactory, new SimulatedExperienceStore<>(descriptionProvider), seedProvider);
          */
 
-        case RELIABILITY -> new RobotCognitionSimulationExecutorFactory(workflowConfiguration, modelLoaderFactory,
-                new SimulatedExperienceStore<>(descriptionProvider), seedProvider);
+//        case RELIABILITY -> new RobotCognitionSimulationExecutorFactory(workflowConfiguration, modelLoaderFactory,
+//                new SimulatedExperienceStore<>(descriptionProvider), seedProvider);
 
         /*
          * case PERFORMABILITY -> new
@@ -34,8 +30,9 @@ public class PcmSimulationExecutorFactory extends BaseSimulationExecutorFactory 
          * modelLoaderFactory, new SimulatedExperienceStore<>(descriptionProvider), seedProvider);
          */
 
-        default -> throw new RuntimeException("Unexpected QualityObjective: " + qualityObjective);
-        };
-        return factory.create();
+//        default -> throw new RuntimeException("Unexpected QualityObjective: " + qualityObjective);
+//        };
+//        return factory.create();
+        throw new RuntimeException("Unsupported factory");
     }
 }
---
+++ examples/org.palladiosimulator.simexp.pcm.examples.hri/src/org/palladiosimulator/simexp/pcm/examples/hri/RobotCognitionLaunchFactory.java
@@ -0,0 +1,51 @@
+package org.palladiosimulator.simexp.pcm.examples.hri;
+
+import java.util.Optional;
+
+import org.palladiosimulator.core.simulation.SimulationExecutor;
+import org.palladiosimulator.simexp.commons.constants.model.QualityObjective;
+import org.palladiosimulator.simexp.commons.constants.model.SimulationEngine;
+import org.palladiosimulator.simexp.commons.constants.model.SimulatorType;
+import org.palladiosimulator.simexp.core.store.SimulatedExperienceStore;
+import org.palladiosimulator.simexp.pcm.config.IWorkflowConfiguration;
+import org.palladiosimulator.simexp.pcm.examples.executor.ModelLoader.Factory;
+import org.palladiosimulator.simexp.pcm.simulator.config.IPCMWorkflowConfiguration;
+import org.palladiosimulator.simexp.workflow.api.ILaunchFactory;
+import org.palladiosimulator.simexp.workflow.api.LaunchDescriptionProvider;
+
+import tools.mdsd.probdist.api.random.ISeedProvider;
+
+public class RobotCognitionLaunchFactory implements ILaunchFactory {
+
+    @Override
+    public boolean canHandle(IWorkflowConfiguration config) {
+        SimulatorType simulatorType = config.getSimulatorType();
+        if (simulatorType != SimulatorType.CUSTOM) {
+            return false;
+        }
+        SimulationEngine simulationEngine = config.getSimulationEngine();
+        if (simulationEngine != SimulationEngine.PCM) {
+            return false;
+        }
+        IPCMWorkflowConfiguration pcmWorkflowConfiguration = (IPCMWorkflowConfiguration) config;
+        QualityObjective qualityObjective = pcmWorkflowConfiguration.getQualityObjective();
+        if (qualityObjective != QualityObjective.RELIABILITY) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    @Override
+    public SimulationExecutor createSimulationExecutor(IWorkflowConfiguration config,
+            LaunchDescriptionProvider descriptionProvider, Optional<ISeedProvider> seedProvider,
+            Factory modelLoaderFactory) {
+        IPCMWorkflowConfiguration workflowConfiguration = (IPCMWorkflowConfiguration) config;
+        RobotCognitionSimulationExecutorFactory factory = new RobotCognitionSimulationExecutorFactory(
+                workflowConfiguration, modelLoaderFactory, new SimulatedExperienceStore<>(descriptionProvider),
+                seedProvider);
+        return factory.create();
+    }
+
+}
