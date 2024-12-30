dd9ee3cb09f7b3c641b8dd3df619f2653c6774c0
Ralf Sieger <sieger@fzi.de>
2024-12-30 09:52:43 +0100

    initial refactoring of launch factories

---
+++ bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/api/ILaunchFactory.java
@@ -0,0 +1,17 @@
+package org.palladiosimulator.simexp.workflow.api;
+
+import java.util.Optional;
+
+import org.palladiosimulator.core.simulation.SimulationExecutor;
+import org.palladiosimulator.simexp.pcm.config.IWorkflowConfiguration;
+import org.palladiosimulator.simexp.pcm.examples.executor.ModelLoader;
+
+import tools.mdsd.probdist.api.random.ISeedProvider;
+
+public interface ILaunchFactory {
+    boolean canHandle(IWorkflowConfiguration config);
+
+    SimulationExecutor createSimulationExecutor(IWorkflowConfiguration config,
+            LaunchDescriptionProvider launchDescriptionProvider, Optional<ISeedProvider> seedProvider,
+            ModelLoader.Factory modelLoaderFactory);
+}
