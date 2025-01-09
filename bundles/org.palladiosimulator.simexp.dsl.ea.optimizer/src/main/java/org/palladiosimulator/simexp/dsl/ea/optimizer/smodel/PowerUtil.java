72bb41e1448eeb300a656a47928695e37fa2bf6e
niklas5810 <niklasbrning@yahoo.de>
2025-01-09 18:12:12 +0100

    Fixed bug that power 0 occuring in edge cases could lead to exceptions

--- bundles/org.palladiosimulator.simexp.dsl.ea.optimizer/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/smodel/PowerUtil.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.optimizer/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/smodel/PowerUtil.java
@@ -2,6 +2,9 @@ package org.palladiosimulator.simexp.dsl.ea.optimizer.smodel;
 
 public class PowerUtil {
     public int minBitSizeForPower(int power) {
+        if (power == 0) {
+            return 1;
+        }
         int minSize = (int) Math.ceil(Math.log(power) / Math.log(2));
         return minSize;
     }
