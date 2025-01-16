abd50ce8c9ada9bf468ca7488d3531c74b6ce171
Ralf Sieger <sieger@fzi.de>
2025-01-16 09:44:30 +0100

    separated read, write store access

--- bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/store/SimulatedExperienceAccessor.java
+++ bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/store/SimulatedExperienceAccessor.java
@@ -1,26 +1,10 @@
 package org.palladiosimulator.simexp.core.store;
 
-import java.util.List;
-import java.util.Optional;
+public interface SimulatedExperienceAccessor extends SimulatedExperienceWriteAccessor, SimulatedExperienceReadAccessor {
 
-import org.palladiosimulator.simexp.core.entity.SimulatedExperience;
+    void setOptionalCache(SimulatedExperienceCache cache);
 
-public interface SimulatedExperienceAccessor {
-	
-	public void setOptionalCache(SimulatedExperienceCache cache);
-	
-	public void connect(SimulatedExperienceStoreDescription desc);
-	
-	public void close();
-	
-	public void store(List<SimulatedExperience> trajectory);
-	
-	public void store(SimulatedExperience simulatedExperience);
-	
-	public Optional<SimulatedExperience> findSimulatedExperience(String id);
-	
-	public Optional<SimulatedExperience> findSelfAdaptiveSystemState(String id);
-	
-	public Optional<List<SimulatedExperience>> getTrajectoryAt(int index);
-	
+    void connect(SimulatedExperienceStoreDescription desc);
+
+    void close();
 }
---
+++ bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/store/SimulatedExperienceReadAccessor.java
@@ -0,0 +1,14 @@
+package org.palladiosimulator.simexp.core.store;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.palladiosimulator.simexp.core.entity.SimulatedExperience;
+
+public interface SimulatedExperienceReadAccessor {
+    Optional<SimulatedExperience> findSimulatedExperience(String id);
+
+    Optional<SimulatedExperience> findSelfAdaptiveSystemState(String id);
+
+    Optional<List<SimulatedExperience>> getTrajectoryAt(int index);
+}
