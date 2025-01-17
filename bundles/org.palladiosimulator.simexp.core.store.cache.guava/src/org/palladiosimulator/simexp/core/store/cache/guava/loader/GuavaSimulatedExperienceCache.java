e90e6f12d820e1d56dbd1a98658a040e60864016
Ralf Sieger <sieger@fzi.de>
2025-01-17 10:16:54 +0100

    removed now superfluous SimulatedExperienceCache interface

--- bundles/org.palladiosimulator.simexp.core.store.cache.guava/src/org/palladiosimulator/simexp/core/store/cache/guava/loader/GuavaSimulatedExperienceCache.java
+++ bundles/org.palladiosimulator.simexp.core.store.cache.guava/src/org/palladiosimulator/simexp/core/store/cache/guava/loader/GuavaSimulatedExperienceCache.java
@@ -3,44 +3,41 @@ package org.palladiosimulator.simexp.core.store.cache.guava.loader;
 import java.util.Optional;
 
 import org.palladiosimulator.simexp.core.entity.SimulatedExperience;
-import org.palladiosimulator.simexp.core.store.SimulatedExperienceCache;
 
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
-public class GuavaSimulatedExperienceCache implements SimulatedExperienceCache {
+public class GuavaSimulatedExperienceCache {
 
-	private final static long CACHE_SIZE = 1000;
-	
-	private final LoadingCache<String, SimulatedExperience> cache;
-	
-	public GuavaSimulatedExperienceCache() {
-		this.cache = CacheBuilder.newBuilder()
-				.maximumSize(CACHE_SIZE)
-				.build(new CacheLoader<String, SimulatedExperience>() {
+    private final static long CACHE_SIZE = 1000;
 
-					@Override
-					public SimulatedExperience load(String id) throws Exception {
-						//TODO exception handling
-						throw new Exception();
-					}
-				});											
-	}
-	
-	@Override
-	public Optional<SimulatedExperience> load(String id) {
-		try {
-			return Optional.of(cache.get(id));
-		} catch (Exception e) {
-			// TODO logging
-			return Optional.empty();
-		}
-	}
+    private final LoadingCache<String, SimulatedExperience> cache;
 
-	@Override
-	public void put(String id, SimulatedExperience simulatedExperience) {
-		cache.put(id, simulatedExperience);
-	}
+    public GuavaSimulatedExperienceCache() {
+        this.cache = CacheBuilder.newBuilder()
+            .maximumSize(CACHE_SIZE)
+            .build(new CacheLoader<String, SimulatedExperience>() {
+
+                @Override
+                public SimulatedExperience load(String id) throws Exception {
+                    // TODO exception handling
+                    throw new Exception();
+                }
+            });
+    }
+
+    public Optional<SimulatedExperience> load(String id) {
+        try {
+            return Optional.of(cache.get(id));
+        } catch (Exception e) {
+            // TODO logging
+            return Optional.empty();
+        }
+    }
+
+    public void put(String id, SimulatedExperience simulatedExperience) {
+        cache.put(id, simulatedExperience);
+    }
 
 }
