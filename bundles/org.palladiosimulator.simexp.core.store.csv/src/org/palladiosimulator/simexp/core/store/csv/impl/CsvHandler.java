99e2bbcff25ba4f5402e335140d23242c6aeb6c5
Ralf Sieger <sieger@fzi.de>
2025-01-17 08:56:39 +0100

    removed now superfluous constant

--- bundles/org.palladiosimulator.simexp.core.store.csv/src/org/palladiosimulator/simexp/core/store/csv/impl/CsvHandler.java
+++ bundles/org.palladiosimulator.simexp.core.store.csv/src/org/palladiosimulator/simexp/core/store/csv/impl/CsvHandler.java
@@ -1,20 +1,10 @@
 package org.palladiosimulator.simexp.core.store.csv.impl;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.eclipse.core.resources.ResourcesPlugin;
-
 public abstract class CsvHandler {
 
     private final static String CSV_FILE_EXTENSION = ".csv";
     public final static String SAMPLE_SPACE_FILE = "SampleSpace" + CSV_FILE_EXTENSION;
     public final static String SIMULATED_EXPERIENCE_STORE_FILE = "SimulatedExperienceStore" + CSV_FILE_EXTENSION;
-
-    public final static Path SIMULATED_EXPERIENCE_BASE_FOLDER = Paths.get(ResourcesPlugin.getWorkspace()
-        .getRoot()
-        .getLocation()
-        .toString() + "/resource");
 
     public abstract void close();
 
