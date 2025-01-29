fe6e626d50365424bc48a8f00651f6a7fbf66c92
Ralf Sieger <sieger@fzi.de>
2025-01-29 13:25:36 +0100

    added basic EA settings

--- bundles/org.palladiosimulator.simexp.dsl.ea.launch/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/EAConfig.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.launch/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/EAConfig.java
@@ -1,7 +1,24 @@
 package org.palladiosimulator.simexp.dsl.ea.launch;
 
+import java.util.Optional;
+
 import org.palladiosimulator.simexp.dsl.ea.api.IEAConfig;
 
 public class EAConfig implements IEAConfig {
+
+    @Override
+    public int populationSize() {
+        return 100;
+    }
+
+    @Override
+    public Optional<Integer> maxGenerations() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Integer> steadyFitness() {
+        return Optional.empty();
+    }
 
 }
