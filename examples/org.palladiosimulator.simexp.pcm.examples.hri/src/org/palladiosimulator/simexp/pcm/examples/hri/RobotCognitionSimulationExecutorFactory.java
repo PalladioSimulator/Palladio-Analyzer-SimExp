4aefd32b70099e79c9bed9dd0d816546836be698
Ralf Sieger <sieger@fzi.de>
2025-02-14 18:24:35 +0100

    adapted to moved AbstractPCMWorkflowRunConfiguration

--- examples/org.palladiosimulator.simexp.pcm.examples.hri/src/org/palladiosimulator/simexp/pcm/examples/hri/RobotCognitionSimulationExecutorFactory.java
+++ examples/org.palladiosimulator.simexp.pcm.examples.hri/src/org/palladiosimulator/simexp/pcm/examples/hri/RobotCognitionSimulationExecutorFactory.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.eclipse.emf.common.util.URI;
-import org.palladiosimulator.analyzer.workflow.ConstantsContainer;
+import org.palladiosimulator.analyzer.workflow.core.ConstantsContainer;
 import org.palladiosimulator.dependability.reliability.uncertainty.UncertaintyRepository;
 import org.palladiosimulator.dependability.reliability.uncertainty.solver.api.UncertaintyBasedReliabilityPredictionConfig;
 import org.palladiosimulator.envdyn.api.entity.bn.DynamicBayesianNetwork;
