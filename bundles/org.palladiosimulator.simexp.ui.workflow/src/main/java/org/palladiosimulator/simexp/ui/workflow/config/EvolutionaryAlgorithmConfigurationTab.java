837586422487a775038f5f242ee754e6977fb60c
Ralf Sieger <sieger@fzi.de>
2025-02-28 13:53:27 +0100

    added population size EA configuration

--- bundles/org.palladiosimulator.simexp.ui.workflow/src/main/java/org/palladiosimulator/simexp/ui/workflow/config/EvolutionaryAlgorithmConfigurationTab.java
+++ bundles/org.palladiosimulator.simexp.ui.workflow/src/main/java/org/palladiosimulator/simexp/ui/workflow/config/EvolutionaryAlgorithmConfigurationTab.java
@@ -1,11 +1,25 @@
 package org.palladiosimulator.simexp.ui.workflow.config;
 
+import org.eclipse.core.databinding.Binding;
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.jface.databinding.fieldassist.ControlDecorationSupport;
+import org.eclipse.jface.databinding.swt.typed.WidgetProperties;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.palladiosimulator.simexp.commons.constants.model.SimulationConstants;
+import org.palladiosimulator.simexp.ui.workflow.config.databinding.ConfigurationProperties;
+import org.palladiosimulator.simexp.ui.workflow.config.databinding.validation.MinIntegerValidator;
 import org.palladiosimulator.simexp.ui.workflow.config.debug.BaseLaunchConfigurationTab;
 
 import de.uka.ipd.sdq.workflow.launchconfig.ImageRegistryHelper;
@@ -13,6 +27,8 @@ import de.uka.ipd.sdq.workflow.launchconfig.ImageRegistryHelper;
 public class EvolutionaryAlgorithmConfigurationTab extends BaseLaunchConfigurationTab {
     private static final String PLUGIN_ID = "org.palladiosimulator.analyzer.workflow";
     private static final String CONFIGURATION_TAB_IMAGE_PATH = "icons/configuration_tab.gif";
+
+    private Text textPopulationSize;
 
     public EvolutionaryAlgorithmConfigurationTab(DataBindingContext ctx) {
         super(ctx);
@@ -22,6 +38,30 @@ public class EvolutionaryAlgorithmConfigurationTab extends BaseLaunchConfigurati
     public void doCreateControl(Composite parent, DataBindingContext ctx) {
         ModifyListener modifyListener = new SimExpModifyListener();
 
+        Composite container = new Composite(parent, SWT.NONE);
+        setControl(container);
+        container.setLayout(new GridLayout());
+
+        Composite simContainer = new Composite(container, SWT.NONE);
+        simContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+        GridLayout simContainerLayout = new GridLayout(2, false);
+        simContainerLayout.marginWidth = 0;
+        simContainer.setLayout(simContainerLayout);
+
+        createBasics(simContainer, modifyListener);
+    }
+
+    private void createBasics(Composite parent, ModifyListener modifyListener) {
+        Group container = new Group(parent, SWT.NONE);
+        container.setText("Basics");
+        container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+        container.setLayout(new GridLayout(2, false));
+
+        final Label simulationIDLabel = new Label(container, SWT.NONE);
+        simulationIDLabel.setText("Population size:");
+        textPopulationSize = new Text(container, SWT.BORDER);
+        textPopulationSize.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+        textPopulationSize.addModifyListener(modifyListener);
     }
 
     @Override
@@ -31,6 +71,21 @@ public class EvolutionaryAlgorithmConfigurationTab extends BaseLaunchConfigurati
 
     @Override
     protected void doInitializeFrom(ILaunchConfigurationWorkingCopy configuration, DataBindingContext ctx) {
+        initializeBasicsFrom(configuration, ctx);
+    }
+
+    private void initializeBasicsFrom(ILaunchConfiguration configuration, DataBindingContext ctx) {
+        IObservableValue<String> numberOfRunsTarget = WidgetProperties.text(SWT.Modify)
+            .observe(textPopulationSize);
+        IObservableValue<Integer> numberOfRunsModel = ConfigurationProperties
+            .integer(SimulationConstants.POPULATION_SIZE)
+            .observe(configuration);
+        UpdateValueStrategy<String, Integer> numberOfRunsUpdateStrategy = new UpdateValueStrategy<>(
+                UpdateValueStrategy.POLICY_CONVERT);
+        numberOfRunsUpdateStrategy.setBeforeSetValidator(new MinIntegerValidator("Population size", 1));
+        Binding numberOfRunsBindValue = ctx.bindValue(numberOfRunsTarget, numberOfRunsModel, numberOfRunsUpdateStrategy,
+                null);
+        ControlDecorationSupport.create(numberOfRunsBindValue, SWT.TOP | SWT.RIGHT);
     }
 
     @Override
