f62881f53bb661b2950c094c93ebcb531b6b2e4a
Ralf Sieger <sieger@fzi.de>
2025-03-02 12:56:25 +0100

    added basic support for double values

---
+++ bundles/org.palladiosimulator.simexp.ui.workflow/src/main/java/org/palladiosimulator/simexp/ui/workflow/config/databinding/ConfigurationObservableDoubleValue.java
@@ -0,0 +1,53 @@
+package org.palladiosimulator.simexp.ui.workflow.config.databinding;
+
+import org.eclipse.core.databinding.observable.value.AbstractObservableValue;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+
+class ConfigurationObservableDoubleValue extends AbstractObservableValue<Double> {
+
+    private final ILaunchConfiguration configuration;
+    private final String key;
+    private final boolean isPrimitive;
+
+    public ConfigurationObservableDoubleValue(ILaunchConfiguration configuration, String key, boolean isPrimitive) {
+        this.configuration = configuration;
+        this.key = key;
+        this.isPrimitive = isPrimitive;
+    }
+
+    @Override
+    public Object getValueType() {
+        return Double.class;
+    }
+
+    @Override
+    protected Double doGetValue() {
+        try {
+            if (!configuration.hasAttribute(key)) {
+                if (!isPrimitive) {
+                    return null;
+                }
+            }
+            String stringValue = configuration.getAttribute(key, "0");
+            return Double.valueOf(stringValue);
+        } catch (CoreException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doSetValue(Double value) {
+        if (configuration instanceof ILaunchConfigurationWorkingCopy) {
+            ILaunchConfigurationWorkingCopy launchConfigurationWorkingCopy = (ILaunchConfigurationWorkingCopy) configuration;
+            String stringValue = null;
+            if (value != null) {
+                stringValue = Double.toString(value);
+            }
+            launchConfigurationWorkingCopy.setAttribute(key, stringValue);
+        } else {
+            throw new RuntimeException("not supported");
+        }
+    }
+}
