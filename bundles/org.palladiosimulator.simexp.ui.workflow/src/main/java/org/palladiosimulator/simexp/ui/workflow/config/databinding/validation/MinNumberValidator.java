e6cf2d6386a12c54c91e22770f860ad4d4706ca0
Ralf Sieger <sieger@fzi.de>
2025-03-02 13:31:57 +0100

    small optimization

--- bundles/org.palladiosimulator.simexp.ui.workflow/src/main/java/org/palladiosimulator/simexp/ui/workflow/config/databinding/validation/MinNumberValidator.java
+++ bundles/org.palladiosimulator.simexp.ui.workflow/src/main/java/org/palladiosimulator/simexp/ui/workflow/config/databinding/validation/MinNumberValidator.java
@@ -7,25 +7,27 @@ import org.eclipse.core.databinding.validation.ValidationStatus;
 import org.eclipse.core.runtime.IStatus;
 
 public class MinNumberValidator<N extends Number & Comparable<N>> implements IValidator<N> {
+
+    class NumberComparator implements Comparator<N> {
+
+        @Override
+        public int compare(N a, N b) throws ClassCastException {
+            return a.compareTo(b);
+        }
+    }
+
     private final String field;
     private final N minValue;
+    private final NumberComparator nc;
 
     public MinNumberValidator(String field, N minValue) {
         this.field = field;
         this.minValue = minValue;
+        this.nc = new NumberComparator();
     }
 
     @Override
     public IStatus validate(N value) {
-        class NumberComparator implements Comparator<N> {
-
-            @Override
-            public int compare(N a, N b) throws ClassCastException {
-                return a.compareTo(b);
-            }
-        }
-
-        NumberComparator nc = new NumberComparator();
         if ((value == null) || (nc.compare(minValue, value) > 0)) {
             return ValidationStatus.error(String.format("%s minimum value is %s", field, minValue));
         }
