a6d6a9f53363f2b0bc93ca6a5ffefb5a8e086775
Ralf Sieger <sieger@fzi.de>
2025-03-04 13:17:00 +0100

    removed default settings for cluster URL

--- bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/preferences/impl/KubernetesPreferenceInitializer.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/preferences/impl/KubernetesPreferenceInitializer.java
@@ -1,17 +1,12 @@
 package org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.preferences.impl;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.core.runtime.preferences.DefaultScope;
-import org.osgi.service.prefs.Preferences;
-import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.preferences.KubernetesPreferenceConstants;
 
 public class KubernetesPreferenceInitializer extends AbstractPreferenceInitializer {
 
     @Override
     public void initializeDefaultPreferences() {
-        Preferences defaults = DefaultScope.INSTANCE.getNode(KubernetesPreferenceConstants.ID);
-
-        defaults.put(KubernetesPreferenceConstants.CLUSTER_URL, "http://something");
+        // Preferences defaults = DefaultScope.INSTANCE.getNode(KubernetesPreferenceConstants.ID);
     }
 
 }
