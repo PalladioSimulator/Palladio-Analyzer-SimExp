10d8fc8fbcbb8e7f4cfa6532ce97f076e2681fd9
Ralf Sieger <sieger@fzi.de>
2025-04-24 15:29:12 +0200

    removed debug code

--- bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/deployment/PodRestartObserver.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/deployment/PodRestartObserver.java
@@ -8,9 +8,7 @@ import org.apache.log4j.Logger;
 import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.deployment.IPodRestartListener.Reason;
 
 import io.fabric8.kubernetes.api.model.ContainerState;
-import io.fabric8.kubernetes.api.model.ContainerStateRunning;
 import io.fabric8.kubernetes.api.model.ContainerStateTerminated;
-import io.fabric8.kubernetes.api.model.ContainerStateWaiting;
 import io.fabric8.kubernetes.api.model.ContainerStatus;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodStatus;
@@ -22,12 +20,10 @@ import io.fabric8.kubernetes.client.WatcherException;
 public class PodRestartObserver implements Watcher<Pod>, AutoCloseable {
     private static final Logger LOGGER = Logger.getLogger(PodRestartObserver.class);
 
-    private final ClassLoader classloader;
     private final Watch watch;
     private final List<IPodRestartListener> listeners = new ArrayList<>();
 
-    public PodRestartObserver(ClassLoader classloader, KubernetesClient client) {
-        this.classloader = classloader;
+    public PodRestartObserver(KubernetesClient client) {
         LOGGER.info("pod restart observer start");
         watch = client.pods()
             .inNamespace("default")
@@ -50,28 +46,16 @@ public class PodRestartObserver implements Watcher<Pod>, AutoCloseable {
 
     @Override
     public void eventReceived(Action action, Pod pod) {
-        ClassLoader oldContextClassLoader = Thread.currentThread()
-            .getContextClassLoader();
-        Thread.currentThread()
-            .setContextClassLoader(classloader);
-        try {
-
-            String podName = pod.getMetadata()
-                .getName();
-            String nodeName = pod.getSpec()
-                .getNodeName();
-            PodStatus podStatus = pod.getStatus();
-            List<ContainerStatus> statuses = podStatus.getContainerStatuses();
-            LOGGER.warn(String.format("pod %s event: %s status %s(%s) count: %d", podName, action,
-                    podStatus.getMessage(), podStatus.getReason(), statuses.size()));
-            for (ListIterator<ContainerStatus> it = statuses.listIterator(); it.hasNext();) {
-                int index = it.nextIndex();
-                ContainerStatus status = it.next();
-                checkRestart(nodeName, podName, index, status);
-            }
-        } finally {
-            Thread.currentThread()
-                .setContextClassLoader(oldContextClassLoader);
+        String podName = pod.getMetadata()
+            .getName();
+        String nodeName = pod.getSpec()
+            .getNodeName();
+        PodStatus podStatus = pod.getStatus();
+        List<ContainerStatus> statuses = podStatus.getContainerStatuses();
+        for (ListIterator<ContainerStatus> it = statuses.listIterator(); it.hasNext();) {
+            int index = it.nextIndex();
+            ContainerStatus status = it.next();
+            checkRestart(nodeName, podName, index, status);
         }
     }
 
@@ -80,51 +64,14 @@ public class PodRestartObserver implements Watcher<Pod>, AutoCloseable {
         if (restartCount > 0) {
             ContainerState lastState = status.getLastState();
             final Reason reason;
-            final String terminatedAt;
             if (lastState != null) {
                 ContainerStateTerminated terminated = lastState.getTerminated();
                 reason = getReason(terminated);
-                terminatedAt = terminated.getFinishedAt();
             } else {
                 reason = getReason(null);
-                terminatedAt = "n/a";
             }
-            String containerName = status.getName();
-            String containerState = getContainerState(status);
-            LOGGER.warn(String.format("pod %s, container %s restarted (state %s) at %s %d times (index: %d)", podName,
-                    containerName, containerState, terminatedAt, restartCount, index));
             notifyListeners(nodeName, podName, reason, restartCount);
         }
-    }
-
-    private String getContainerState(ContainerStatus status) {
-        ContainerState containerState = status.getState();
-        StringBuilder sb = new StringBuilder();
-        ContainerStateRunning running = containerState.getRunning();
-        if (running != null) {
-            sb.append("running");
-            sb.append(" since ")
-                .append(running.getStartedAt());
-        }
-        ContainerStateWaiting waiting = containerState.getWaiting();
-        if (waiting != null) {
-            sb.append(" waiting");
-            sb.append(" reason ")
-                .append(waiting.getReason());
-            sb.append(" message ")
-                .append(waiting.getMessage());
-        }
-        ContainerStateTerminated terminated = containerState.getTerminated();
-        if (terminated != null) {
-            sb.append(" terminated");
-            sb.append(" started ")
-                .append(terminated.getStartedAt());
-            sb.append(" terminated ")
-                .append(terminated.getFinishedAt());
-            sb.append(" reason ")
-                .append(terminated.getReason());
-        }
-        return sb.toString();
     }
 
     private Reason getReason(ContainerStateTerminated terminated) {
