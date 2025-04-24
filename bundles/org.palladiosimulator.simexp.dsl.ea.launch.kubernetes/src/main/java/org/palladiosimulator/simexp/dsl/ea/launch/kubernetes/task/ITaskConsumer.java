73af136487cf2de3bd6759864fb4706aedfd3edb
Ralf Sieger <sieger@fzi.de>
2025-04-24 11:15:23 +0200

    adapted parameter names

--- bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/task/ITaskConsumer.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/task/ITaskConsumer.java
@@ -1,7 +1,7 @@
 package org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.task;
 
 public interface ITaskConsumer {
-    void taskStarted(String answerId, JobResult result);
+    void taskStarted(String taskId, JobResult result);
 
-    void taskCompleted(String answerId, JobResult result);
+    void taskCompleted(String taskId, JobResult result);
 }
