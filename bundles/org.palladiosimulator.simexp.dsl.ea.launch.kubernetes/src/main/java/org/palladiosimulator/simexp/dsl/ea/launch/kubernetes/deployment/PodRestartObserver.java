0f9914eb82644ee0cae5b0028794e382d26b5934
Ralf Sieger <sieger@fzi.de>
2025-04-24 10:52:14 +0200

    added enhanced diagnostics

--- bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/deployment/PodRestartObserver.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/deployment/PodRestartObserver.java
@@ -2,12 +2,15 @@ package org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.deployment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.apache.log4j.Logger;
 import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.deployment.IPodRestartListener.Reason;
 
 import io.fabric8.kubernetes.api.model.ContainerState;
+import io.fabric8.kubernetes.api.model.ContainerStateRunning;
 import io.fabric8.kubernetes.api.model.ContainerStateTerminated;
+import io.fabric8.kubernetes.api.model.ContainerStateWaiting;
 import io.fabric8.kubernetes.api.model.ContainerStatus;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodStatus;
@@ -47,26 +50,69 @@ public class PodRestartObserver implements Watcher<Pod>, AutoCloseable {
 
     @Override
     public void eventReceived(Action action, Pod pod) {
+        String podName = pod.getMetadata()
+            .getName();
         PodStatus podStatus = pod.getStatus();
         List<ContainerStatus> statuses = podStatus.getContainerStatuses();
-        for (ContainerStatus status : statuses) {
-            int restartCount = status.getRestartCount();
-            if (restartCount > 0) {
-                String podName = pod.getMetadata()
-                    .getName();
-                LOGGER.warn(String.format("%s: pod %s restarted %d times", action, podName, restartCount));
-                ContainerState lastState = status.getLastState();
-                final Reason reason;
-                if (lastState != null) {
-                    ContainerStateTerminated terminated = lastState.getTerminated();
-                    reason = getReason(terminated);
-                } else {
-                    reason = getReason(null);
-                }
-                notifyListeners(podName, reason);
-                break;
-            }
+        LOGGER.warn(String.format("pod %s event: %s status %s(%s) count: %d", podName, action, podStatus.getMessage(),
+                podStatus.getReason(), statuses.size()));
+        for (ListIterator<ContainerStatus> it = statuses.listIterator(); it.hasNext();) {
+            int index = it.nextIndex();
+            ContainerStatus status = it.next();
+            checkRestart(podName, index, status);
         }
+    }
+
+    private void checkRestart(String podName, int index, ContainerStatus status) {
+        int restartCount = status.getRestartCount();
+        if (restartCount > 0) {
+            ContainerState lastState = status.getLastState();
+            final Reason reason;
+            final String terminatedAt;
+            if (lastState != null) {
+                ContainerStateTerminated terminated = lastState.getTerminated();
+                reason = getReason(terminated);
+                terminatedAt = terminated.getFinishedAt();
+            } else {
+                reason = getReason(null);
+                terminatedAt = "n/a";
+            }
+            String containerName = status.getName();
+            String containerState = getContainerState(status);
+            LOGGER.warn(String.format("pod %s, container %s restarted (state %s) at %s %d times (index: %d)", podName,
+                    containerName, containerState, terminatedAt, restartCount, index));
+            notifyListeners(podName, reason);
+        }
+    }
+
+    private String getContainerState(ContainerStatus status) {
+        ContainerState containerState = status.getState();
+        StringBuilder sb = new StringBuilder();
+        ContainerStateRunning running = containerState.getRunning();
+        if (running != null) {
+            sb.append("running");
+            sb.append(" since ")
+                .append(running.getStartedAt());
+        }
+        ContainerStateWaiting waiting = containerState.getWaiting();
+        if (waiting != null) {
+            sb.append(" waiting");
+            sb.append(" reason ")
+                .append(waiting.getReason());
+            sb.append(" message ")
+                .append(waiting.getMessage());
+        }
+        ContainerStateTerminated terminated = containerState.getTerminated();
+        if (terminated != null) {
+            sb.append(" terminated");
+            sb.append(" started ")
+                .append(terminated.getStartedAt());
+            sb.append(" terminated ")
+                .append(terminated.getFinishedAt());
+            sb.append(" reason ")
+                .append(terminated.getReason());
+        }
+        return sb.toString();
     }
 
     private Reason getReason(ContainerStateTerminated terminated) {
