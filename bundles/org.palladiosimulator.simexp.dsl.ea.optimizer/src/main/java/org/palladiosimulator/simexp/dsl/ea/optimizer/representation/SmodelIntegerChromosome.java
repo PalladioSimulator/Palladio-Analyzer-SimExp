dea502219268a6b54f7f6b34d111086adeb3dea6
niklas5810 <niklasbrning@yahoo.de>
2025-04-24 19:36:33 +0200

    removed unnecessary max field from SmodelIntegerChromosome

--- bundles/org.palladiosimulator.simexp.dsl.ea.optimizer/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/representation/SmodelIntegerChromosome.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.optimizer/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/representation/SmodelIntegerChromosome.java
@@ -13,15 +13,13 @@ import io.jenetics.util.IntRange;
 public class SmodelIntegerChromosome extends IntegerChromosome {
 
     private Optimizable optimizable;
-    private int max;
 
     private int numOfValues;
 
-    protected SmodelIntegerChromosome(ISeq<IntegerGene> genes, IntRange lengthRange, Optimizable optimizable, int max,
+    protected SmodelIntegerChromosome(ISeq<IntegerGene> genes, IntRange lengthRange, Optimizable optimizable,
             int numOfValues) {
         super(genes, lengthRange);
         this.optimizable = optimizable;
-        this.max = max;
         this.numOfValues = numOfValues;
     }
 
@@ -41,25 +39,24 @@ public class SmodelIntegerChromosome extends IntegerChromosome {
             genes.add(integerChromosome.get(i));
         }
 
-        return new SmodelIntegerChromosome(ISeq.of(genes), IntRange.of(1), optimizable, intRange.max(),
-                intRange.size() + 1);
+        return new SmodelIntegerChromosome(ISeq.of(genes), IntRange.of(1), optimizable, intRange.size() + 1);
     }
 
     @Override
     public IntegerChromosome newInstance(final ISeq<IntegerGene> genes) {
-        return new SmodelIntegerChromosome(genes, lengthRange(), optimizable, max, numOfValues);
+        return new SmodelIntegerChromosome(genes, lengthRange(), optimizable, numOfValues);
     }
 
     @Override
     public SmodelIntegerChromosome newInstance() {
 
-        IntegerChromosome integerChromosome = IntegerChromosome.of(0, max);
+        IntegerChromosome integerChromosome = IntegerChromosome.of(min(), max());
         List<IntegerGene> genes = new ArrayList();
         for (int i = 0; i < integerChromosome.length(); i++) {
             genes.add(integerChromosome.get(i));
         }
 
-        return new SmodelIntegerChromosome(ISeq.of(genes), IntRange.of(1), optimizable, max, numOfValues);
+        return new SmodelIntegerChromosome(ISeq.of(genes), IntRange.of(1), optimizable, numOfValues);
     }
 
 }
