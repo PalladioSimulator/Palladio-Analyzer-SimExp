3401e978e83bef641bfdec65f876ebbf2e0f995a
Ralf Sieger <sieger@fzi.de>
2025-06-11 17:12:02 +0200

    simplified the test code

--- tests/org.palladiosimulator.simexp.dsl.ea.optimizer.tests/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/impl/constraints/OptimizableChromosomeBinaryConstraintTest.java
+++ tests/org.palladiosimulator.simexp.dsl.ea.optimizer.tests/src/main/java/org/palladiosimulator/simexp/dsl/ea/optimizer/impl/constraints/OptimizableChromosomeBinaryConstraintTest.java
@@ -1,103 +1,77 @@
 package org.palladiosimulator.simexp.dsl.ea.optimizer.impl.constraints;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
-
-import java.util.Random;
-import java.util.function.Function;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.palladiosimulator.simexp.dsl.ea.optimizer.representation.BinaryBitInterpreter;
 import org.palladiosimulator.simexp.dsl.ea.optimizer.representation.SmodelBitChromosome;
-import org.palladiosimulator.simexp.dsl.ea.optimizer.representation.SmodelBitset;
 import org.palladiosimulator.simexp.dsl.smodel.smodel.Optimizable;
 
 import io.jenetics.BitGene;
 import io.jenetics.Genotype;
 import io.jenetics.Phenotype;
-import io.jenetics.util.RandomRegistry;
 
 public class OptimizableChromosomeBinaryConstraintTest {
+    private OptimizableChromosomeBinaryConstraint objectUnderTest;
 
     @Mock
     private Optimizable optimizable;
+    @Mock
+    private SmodelBitChromosome chromosome;
+    @Mock
+    private SmodelBitChromosome newChromosome;
+
+    private Genotype<BitGene> genotype;
+    private Phenotype<BitGene, Double> phenotype;
 
     @Before
     public void setUp() {
         initMocks(this);
+
+        when(chromosome.isValid()).thenReturn(true);
+        genotype = Genotype.of(chromosome);
+        phenotype = Phenotype.of(genotype, 0);
+
+        objectUnderTest = new OptimizableChromosomeBinaryConstraint();
     }
 
     @Test
     public void testValidPhenotype() {
-        SmodelBitset bitSet = new SmodelBitset(4);
-        bitSet.set(1);
-        SmodelBitChromosome chromosome = SmodelBitChromosome.of(bitSet, optimizable, 5, new BinaryBitInterpreter());
-        Phenotype<BitGene, Double> phenotype = Phenotype.of(Genotype.of(chromosome), 0);
-
-        OptimizableChromosomeBinaryConstraint objectUnderTest = new OptimizableChromosomeBinaryConstraint();
-
         assertTrue(objectUnderTest.test(phenotype));
     }
 
     @Test
     public void testInvalidPhenotype() {
-        SmodelBitset bitSet = new SmodelBitset(4);
-        bitSet.set(3);
-        SmodelBitChromosome chromosome = SmodelBitChromosome.of(bitSet, optimizable, 5, new BinaryBitInterpreter());
-        Phenotype<BitGene, Double> phenotype = Phenotype.of(Genotype.of(chromosome), 0);
-
-        OptimizableChromosomeBinaryConstraint objectUnderTest = new OptimizableChromosomeBinaryConstraint();
+        when(chromosome.isValid()).thenReturn(false);
 
         assertFalse(objectUnderTest.test(phenotype));
     }
 
     @Test
-    public void testRepairPhenotype() {
-        SmodelBitset bitSet = new SmodelBitset(4);
-        bitSet.set(3);
-        SmodelBitset expectedBitset = new SmodelBitset(4);
-        expectedBitset.set(0);
-        SmodelBitChromosome chromosome = SmodelBitChromosome.of(bitSet, optimizable, 5, new BinaryBitInterpreter());
-        Phenotype<BitGene, Double> phenotype = Phenotype.of(Genotype.of(chromosome), 0);
-        OptimizableChromosomeBinaryConstraint objectUnderTest = new OptimizableChromosomeBinaryConstraint();
-        Function<? super Random, Phenotype<BitGene, Double>> optFunction = (Random r) -> {
-            return objectUnderTest.repair(phenotype, 0);
-        };
+    public void testRepairInvalidPhenotype() {
+        when(chromosome.isValid()).thenReturn(false);
+        when(newChromosome.isValid()).thenReturn(true);
+        when(chromosome.newInstance()).thenReturn(newChromosome);
 
-        Phenotype<BitGene, Double> newInstance = RandomRegistry.with(new Random(30), optFunction);
+        Phenotype<BitGene, Double> actualInstance = objectUnderTest.repair(phenotype, 0);
 
-        assertTrue(objectUnderTest.test(newInstance));
-        assertTrue(newInstance.isValid());
-        assertEquals(expectedBitset, newInstance.genotype()
-            .chromosome()
-            .as(SmodelBitChromosome.class)
-            .toBitSet());
+        assertTrue(actualInstance.isValid());
+        assertSame(newChromosome, actualInstance.genotype()
+                .get(0));
     }
 
     @Test
     public void testRepairValidPhenotype() {
-        SmodelBitset bitSet = new SmodelBitset(4);
-        bitSet.set(2);
+        Phenotype<BitGene, Double> actualInstance = objectUnderTest.repair(phenotype, 0);
 
-        SmodelBitChromosome chromosome = SmodelBitChromosome.of(bitSet, optimizable, 5, new BinaryBitInterpreter());
-        Phenotype<BitGene, Double> phenotype = Phenotype.of(Genotype.of(chromosome), 0);
-        OptimizableChromosomeBinaryConstraint objectUnderTest = new OptimizableChromosomeBinaryConstraint();
-        Function<? super Random, Phenotype<BitGene, Double>> optFunction = (Random r) -> {
-            return objectUnderTest.repair(phenotype, 0);
-        };
-
-        Phenotype<BitGene, Double> newInstance = RandomRegistry.with(new Random(30), optFunction);
-
-        assertTrue(objectUnderTest.test(newInstance));
-        assertTrue(newInstance.isValid());
-        assertEquals(bitSet, newInstance.genotype()
-            .chromosome()
-            .as(SmodelBitChromosome.class)
-            .toBitSet());
+        assertTrue(actualInstance.isValid());
+        assertSame(chromosome, actualInstance.genotype()
+            .get(0));
     }
 
 }
