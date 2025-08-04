b79382f2c1cf1b7883da224b6461ad32f210176a
Ralf Sieger <sieger@fzi.de>
2025-08-04 09:33:35 +0200

    removed unused attributes

--- bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/store/SimulatedExperienceStoreDescription.java
+++ bundles/org.palladiosimulator.simexp.core/src/org/palladiosimulator/simexp/core/store/SimulatedExperienceStoreDescription.java
@@ -2,25 +2,13 @@ package org.palladiosimulator.simexp.core.store;
 
 public class SimulatedExperienceStoreDescription {
 
-    private final String simulationId;
-    private final String sampleSpaceId;
     private final int sampleHorizon;
 
-    public SimulatedExperienceStoreDescription(String simulationId, String sampleSpaceId, int sampleHorizon) {
-        this.simulationId = simulationId;
-        this.sampleSpaceId = sampleSpaceId;
+    public SimulatedExperienceStoreDescription(int sampleHorizon) {
         this.sampleHorizon = sampleHorizon;
     }
 
     public int getSampleHorizon() {
         return sampleHorizon;
-    }
-
-    public String getSampleSpaceId() {
-        return sampleSpaceId;
-    }
-
-    public String getSimulationId() {
-        return simulationId;
     }
 }
--- bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/api/LaunchDescriptionProvider.java
+++ bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/api/LaunchDescriptionProvider.java
@@ -2,23 +2,19 @@ package org.palladiosimulator.simexp.workflow.api;
 
 import org.palladiosimulator.simexp.core.store.DescriptionProvider;
 import org.palladiosimulator.simexp.core.store.SimulatedExperienceStoreDescription;
-import org.palladiosimulator.simexp.core.util.SimulatedExperienceConstants;
 import org.palladiosimulator.simexp.pcm.config.SimulationParameters;
 
 public class LaunchDescriptionProvider implements DescriptionProvider {
-    private final String simulationID;
     private final int horizon;
 
     private SimulatedExperienceStoreDescription desc;
 
     public LaunchDescriptionProvider(SimulationParameters simulationParameters) {
-        simulationID = simulationParameters.getSimulationID();
         horizon = simulationParameters.getNumberOfSimulationsPerRun();
     }
 
     public void setPolicyId(String policyId) {
-        String sampleSpaceId = SimulatedExperienceConstants.constructSampleSpaceId(simulationID, policyId);
-        desc = new SimulatedExperienceStoreDescription(simulationID, sampleSpaceId, horizon);
+        desc = new SimulatedExperienceStoreDescription(horizon);
     }
 
     @Override
