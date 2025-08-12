6e413fce633e4e079b345db0ccce62ebdcf318dc
Ralf Sieger <sieger@fzi.de>
2025-08-12 14:46:29 +0200

    added used optimizable paremeters to job results

--- bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/result/json/JsonQualityAttributesResultLogger.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/result/json/JsonQualityAttributesResultLogger.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.result.csv.CsvResultLogger;
+import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.result.json.TaskResult.OptimizableParam;
 import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.task.IResultHandler;
 import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.task.JobResult;
 import org.palladiosimulator.simexp.dsl.smodel.api.OptimizableValue;
@@ -35,15 +37,28 @@ public class JsonQualityAttributesResultLogger implements IResultHandler {
     public synchronized void process(List<OptimizableValue<?>> optimizableValues, JobResult result) {
         Path taskPath = buildTaskPath(result);
         try (Writer writer = Files.newBufferedWriter(taskPath)) {
-            gson.toJson(result, writer);
+            List<OptimizableParam<?>> optimizablePairs = buildOptimizablePairs(optimizableValues);
+            TaskResult taskResult = new TaskResult(optimizablePairs, result);
+            gson.toJson(taskResult, writer);
         } catch (IOException e) {
             LOGGER.error(e.getMessage(), e);
         }
     }
 
+    private List<OptimizableParam<?>> buildOptimizablePairs(List<OptimizableValue<?>> optimizableValues) {
+        List<OptimizableParam<?>> optimizablePairs = new ArrayList<>();
+        for (OptimizableValue<?> optimizable : optimizableValues) {
+            String name = optimizable.getOptimizable()
+                .getName();
+            OptimizableParam<?> param = new OptimizableParam<>(name, optimizable.getValue());
+            optimizablePairs.add(param);
+        }
+        return optimizablePairs;
+    }
+
     private Path buildTaskPath(JobResult result) {
         String taskName = result.id.replaceAll("\\s+", "_");
-        String fileName = String.format("%s.json", taskName);
+        String fileName = String.format("Result_%s.json", taskName);
         Path taskPath = qaPath.resolve(fileName);
         return taskPath;
     }
