88c36ea2dd536a47800543b0042605458ad98ebb
Ralf Sieger <sieger@fzi.de>
2025-08-14 13:39:49 +0200

    simplified result task format

--- bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/result/json/JsonQualityAttributesResultLogger.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/result/json/JsonQualityAttributesResultLogger.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.result.csv.CsvResultLogger;
-import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.result.json.TaskResult.OptimizableParam;
 import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.task.IResultHandler;
 import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.task.JobResult;
 import org.palladiosimulator.simexp.dsl.smodel.api.OptimizableValue;
@@ -37,23 +37,22 @@ public class JsonQualityAttributesResultLogger implements IResultHandler {
     public synchronized void process(List<OptimizableValue<?>> optimizableValues, JobResult result) {
         Path taskPath = buildTaskPath(result);
         try (Writer writer = Files.newBufferedWriter(taskPath)) {
-            List<OptimizableParam<?>> optimizablePairs = buildOptimizablePairs(optimizableValues);
-            TaskResult taskResult = new TaskResult(optimizablePairs, result);
+            Map<String, Object> optimizables = buildOptimizablePairs(optimizableValues);
+            TaskResult taskResult = new TaskResult(optimizables, result);
             gson.toJson(taskResult, writer);
         } catch (IOException e) {
             LOGGER.error(e.getMessage(), e);
         }
     }
 
-    private List<OptimizableParam<?>> buildOptimizablePairs(List<OptimizableValue<?>> optimizableValues) {
-        List<OptimizableParam<?>> optimizablePairs = new ArrayList<>();
+    private Map<String, Object> buildOptimizablePairs(List<OptimizableValue<?>> optimizableValues) {
+        Map<String, Object> optimizables = new HashMap<>();
         for (OptimizableValue<?> optimizable : optimizableValues) {
             String name = optimizable.getOptimizable()
                 .getName();
-            OptimizableParam<?> param = new OptimizableParam<>(name, optimizable.getValue());
-            optimizablePairs.add(param);
+            optimizables.put(name, optimizable.getValue());
         }
-        return optimizablePairs;
+        return optimizables;
     }
 
     private Path buildTaskPath(JobResult result) {
--- bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/result/json/TaskResult.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.launch.kubernetes/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/kubernetes/result/json/TaskResult.java
@@ -1,26 +1,15 @@
 package org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.result.json;
 
-import java.util.List;
+import java.util.Map;
 
 import org.palladiosimulator.simexp.dsl.ea.launch.kubernetes.task.JobResult;
 
 public class TaskResult {
-    public static class OptimizableParam<V> {
-        public final String name;
-        public final V value;
-
-        public OptimizableParam(String name, V value) {
-            this.name = name;
-            this.value = value;
-        }
-    }
-
-    public final List<OptimizableParam<?>> optimizables;
-
+    public final Map<String, Object> optimizables;
     public final JobResult result;
 
-    public TaskResult(List<OptimizableParam<?>> optimizablePairs, JobResult result) {
-        this.optimizables = optimizablePairs;
+    public TaskResult(Map<String, Object> optimizables, JobResult result) {
+        this.optimizables = optimizables;
         this.result = result;
     }
 }
