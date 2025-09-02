e8aaf1474b9730310e39466fc28d4e993b40431a
Ralf Sieger <sieger@fzi.de>
2025-09-02 10:58:15 +0200

    added tests framework for org.palladiosimulator.simexp.distribution.tests

---
+++ tests/org.palladiosimulator.simexp.distribution.tests/src/main/java/org/palladiosimulator/simexp/distribution/DummyTest.java
@@ -0,0 +1,12 @@
+package org.palladiosimulator.simexp.distribution;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void removeMe() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
