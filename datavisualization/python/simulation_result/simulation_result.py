9fc2622e76c67c2c7130278feb889b92366dda08
Ralf Sieger <sieger@fzi.de>
2025-09-15 08:40:44 +0200

    initial statistics

--- datavisualization/python/simulation_result/simulation_result.py
+++ datavisualization/python/simulation_result/simulation_result.py
@@ -1,13 +1,32 @@
 import sys
 import argparse
+import csv
+import collections
 
 class SimulationResult:
-  def main(self):
-    parser = argparse.ArgumentParser(prog="simulation_result", description="Analyses simulation results")
-    parser.add_argument('infile', type=argparse.FileType('r'))
-    args = parser.parse_args()
 
-    print("validate input...")
+    def _read_file(self, csv_file):
+        fieldnames = ["TaskId", "Values", "Reward", "Error", "ExecutorId"]
+        reader = csv.DictReader(csv_file, fieldnames=fieldnames, delimiter=";")
+        next(reader) # skip header
+        return reader
+
+    def main(self):
+        parser = argparse.ArgumentParser(prog="simulation_result", description="Analyses simulation results")
+        parser.add_argument('infile', type=argparse.FileType('r'))
+        args = parser.parse_args()
+
+        print("Statistics:")
+        content = self._read_file(args.infile)
+        counter = collections.Counter()
+        for row in content:
+            counter[row['Error']] += 1
+        for key, count in counter.items():
+            if key:
+                print("error: %s: %s" % (key, count))
+        print("total entries: %s" % counter.total())
+
+        return 0
 
 if __name__ == '__main__':
     sr = SimulationResult()
