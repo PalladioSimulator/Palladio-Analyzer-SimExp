d41dfcf5af804bd9810937d1d573f3771368e275
Ralf Sieger <sieger@fzi.de>
2025-09-17 11:45:20 +0200

    introduced IRewardRounder interface

--- bundles/org.palladiosimulator.simexp.dsl.ea.api/src/main/java/org/palladiosimulator/simexp/dsl/ea/api/util/RewardFormater.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.api/src/main/java/org/palladiosimulator/simexp/dsl/ea/api/util/RewardFormater.java
@@ -2,12 +2,13 @@ package org.palladiosimulator.simexp.dsl.ea.api.util;
 
 import org.palladiosimulator.simexp.dsl.smodel.api.PrecisionProvider;
 
-public class RewardFormater extends PrecisionProvider implements IRewardFormater {
+public class RewardFormater extends PrecisionProvider implements IRewardRounder, IRewardFormater {
 
     public RewardFormater(int places) {
         super(places);
     }
 
+    @Override
     public double round(double reward) {
         double multiplicator = (long) (1.0 / getPrecision());
         return Math.round(reward * multiplicator) / multiplicator;
