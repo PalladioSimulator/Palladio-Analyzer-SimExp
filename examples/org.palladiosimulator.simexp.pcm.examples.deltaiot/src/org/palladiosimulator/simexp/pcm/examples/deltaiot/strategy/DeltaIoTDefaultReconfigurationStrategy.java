220b9e76c83d44217b74d4bd104af97aeb7d9a3b
Ralf Sieger <sieger@fzi.de>
2025-11-30 12:54:26 +0100

    only adjust power or distribution factor according to DeltaIoT examplar

--- examples/org.palladiosimulator.simexp.pcm.examples.deltaiot/src/org/palladiosimulator/simexp/pcm/examples/deltaiot/strategy/DeltaIoTDefaultReconfigurationStrategy.java
+++ examples/org.palladiosimulator.simexp.pcm.examples.deltaiot/src/org/palladiosimulator/simexp/pcm/examples/deltaiot/strategy/DeltaIoTDefaultReconfigurationStrategy.java
@@ -108,19 +108,23 @@ public class DeltaIoTDefaultReconfigurationStrategy
         }
 
         MoteContextFilter moteFiler = new MoteContextFilter(knowledge);
+        boolean powerChanging = false;
         for (MoteContext eachMote : moteFiler.getAllMoteContexts()) {
             for (WirelessLink eachLink : eachMote.links) {
+                powerChanging = false;
                 if (customizer instanceof ITransmissionPowerReconfiguration) {
                     ITransmissionPowerReconfiguration transmissionPowerReconfiguration = (ITransmissionPowerReconfiguration) customizer;
                     if (eachLink.SNR > 0 && eachLink.transmissionPower > 0) {
                         decreaseTransmissionPower(eachMote.mote, eachLink, transmissionPowerReconfiguration);
+                        powerChanging = true;
                     } else if (eachLink.SNR < 0 && eachLink.transmissionPower < 15) {
                         increaseTransmissionPower(eachMote.mote, eachLink, transmissionPowerReconfiguration);
+                        powerChanging = true;
                     }
                 }
             }
 
-            if (eachMote.hasTwoLinks()) {
+            if (eachMote.hasTwoLinks() && (powerChanging == false)) {
                 if (customizer instanceof IDistributionFactorReconfiguration) {
                     IDistributionFactorReconfiguration distributionFactorReconfiguration = (IDistributionFactorReconfiguration) customizer;
 
