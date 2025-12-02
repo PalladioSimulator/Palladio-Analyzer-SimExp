86222023f8885492c83503409681cd5d2091eb6b
Ralf Sieger <sieger@fzi.de>
2025-12-02 14:23:07 +0100

    added json debug log statements

--- bundles/org.palladiosimulator.simexp.dsl.ea.launch/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/log/GenerationParetoFrontBuilder.java
+++ bundles/org.palladiosimulator.simexp.dsl.ea.launch/src/main/java/org/palladiosimulator/simexp/dsl/ea/launch/log/GenerationParetoFrontBuilder.java
@@ -12,6 +12,10 @@ import org.palladiosimulator.simexp.dsl.ea.pareto.ParetoFrontBuilder;
 import org.palladiosimulator.simexp.dsl.smodel.api.IPrecisionProvider;
 import org.palladiosimulator.simexp.dsl.smodel.api.OptimizableValue;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class GenerationParetoFrontBuilder implements IEAEvolutionStatusReceiver {
     private final Path generationsPath;
     private final IQualityAttributeProvider qualityAttributeProvider;
@@ -30,8 +34,32 @@ public class GenerationParetoFrontBuilder implements IEAEvolutionStatusReceiver 
         Path generationFile = generationsPath.resolve(String.format("pareto_front_%03d.json", generation));
         ParetoFrontBuilder paretoFrontBuilder = new ParetoFrontBuilder(qualityAttributeProvider, precisionProvider);
         List<IndividualParetoResult> paretoFront = paretoFrontBuilder.buildParetoFront(population);
-        JsonResultWriter jsonParetoWriter = new JsonResultWriter();
-        jsonParetoWriter.storeIndividualParetoResults(generationFile, paretoFront);
+        try {
+            JsonResultWriter jsonParetoWriter = new JsonResultWriter();
+            jsonParetoWriter.storeIndividualParetoResults(generationFile, paretoFront);
+        } catch (IllegalArgumentException e) {
+            Gson gson = new GsonBuilder() //
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .serializeNulls()
+                .serializeSpecialFloatingPointValues()
+                .setPrettyPrinting()
+                .create();
+            StringBuilder sb = new StringBuilder();
+            try {
+                sb.append("population:\n");
+                sb.append(gson.toJson(population));
+            } catch (Exception e2) {
+                sb.append("exception2: " + e2.getMessage());
+            }
+            try {
+                sb.append("\n");
+                sb.append("pareto front:\n");
+                sb.append(gson.toJson(paretoFront));
+            } catch (Exception e3) {
+                sb.append("exception3: " + e3.getMessage());
+            }
+            throw new RuntimeException(String.format("causing front:\n%s", sb.toString()), e);
+        }
     }
 
     @Override
