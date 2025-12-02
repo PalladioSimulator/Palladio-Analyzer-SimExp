34aa3def234b937c192a3aee0faeb23dcb2cd508
Ralf Sieger <sieger@fzi.de>
2025-12-02 09:50:35 +0100

    added boundary tests

--- tests/org.palladiosimulator.simexp.pcm.examples.deltaiot.tests/src/main/java/org/palladiosimulator/simexp/pcm/examples/deltaiot/reward/QualityBasedRewardEvaluatorTest.java
+++ tests/org.palladiosimulator.simexp.pcm.examples.deltaiot.tests/src/main/java/org/palladiosimulator/simexp/pcm/examples/deltaiot/reward/QualityBasedRewardEvaluatorTest.java
@@ -15,6 +15,7 @@ import org.palladiosimulator.simexp.core.entity.SimulatedMeasurement;
 import org.palladiosimulator.simexp.core.entity.SimulatedMeasurementSpecification;
 import org.palladiosimulator.simexp.core.state.StateQuantity;
 import org.palladiosimulator.simexp.markovian.model.markovmodel.markoventity.Reward;
+import org.palladiosimulator.simexp.pcm.examples.deltaiot.util.DeltaIoTCommons;
 
 public class QualityBasedRewardEvaluatorTest {
     private static final double EPSILON = 0.0001;
@@ -37,7 +38,7 @@ public class QualityBasedRewardEvaluatorTest {
     }
 
     @Test
-    public void testEvaluate() {
+    public void testEvaluateInRange() {
         SimulatedMeasurement packetLossMeasurement = SimulatedMeasurement.of(0.06, packetLossSpec);
         SimulatedMeasurement energyConsumptionMeasurement = SimulatedMeasurement.of(31.9, energyConsumptionSpec);
         List<SimulatedMeasurement> measuredQuantities = Arrays.asList(packetLossMeasurement,
@@ -49,4 +50,59 @@ public class QualityBasedRewardEvaluatorTest {
         assertThat(actualReward.getValue()).isEqualTo(1.45, withPrecision(EPSILON));
     }
 
+    @Test
+    public void testEvaluatePLLow() {
+        SimulatedMeasurement packetLossMeasurement = SimulatedMeasurement
+            .of(DeltaIoTCommons.LOWER_BOUND_PACKET_LOSS - 0.01, packetLossSpec);
+        SimulatedMeasurement energyConsumptionMeasurement = SimulatedMeasurement.of(31.9, energyConsumptionSpec);
+        List<SimulatedMeasurement> measuredQuantities = Arrays.asList(packetLossMeasurement,
+                energyConsumptionMeasurement);
+        StateQuantity quantifiedState = StateQuantity.of(measuredQuantities);
+
+        Reward<Double> actualReward = evaluator.evaluate(quantifiedState);
+
+        assertThat(actualReward.getValue()).isEqualTo(1.65, withPrecision(EPSILON));
+    }
+
+    @Test
+    public void testEvaluatePLHigh() {
+        SimulatedMeasurement packetLossMeasurement = SimulatedMeasurement
+            .of(DeltaIoTCommons.UPPER_BOUND_PACKET_LOSS + 0.01, packetLossSpec);
+        SimulatedMeasurement energyConsumptionMeasurement = SimulatedMeasurement.of(31.9, energyConsumptionSpec);
+        List<SimulatedMeasurement> measuredQuantities = Arrays.asList(packetLossMeasurement,
+                energyConsumptionMeasurement);
+        StateQuantity quantifiedState = StateQuantity.of(measuredQuantities);
+
+        Reward<Double> actualReward = evaluator.evaluate(quantifiedState);
+
+        assertThat(actualReward.getValue()).isEqualTo(0.65, withPrecision(EPSILON));
+    }
+
+    @Test
+    public void testEvaluateECLow() {
+        SimulatedMeasurement packetLossMeasurement = SimulatedMeasurement.of(0.06, packetLossSpec);
+        SimulatedMeasurement energyConsumptionMeasurement = SimulatedMeasurement
+            .of(DeltaIoTCommons.LOWER_BOUND_ENERGY_CONSUMPTION - 0.1, energyConsumptionSpec);
+        List<SimulatedMeasurement> measuredQuantities = Arrays.asList(packetLossMeasurement,
+                energyConsumptionMeasurement);
+        StateQuantity quantifiedState = StateQuantity.of(measuredQuantities);
+
+        Reward<Double> actualReward = evaluator.evaluate(quantifiedState);
+
+        assertThat(actualReward.getValue()).isEqualTo(1.7999, withPrecision(EPSILON));
+    }
+
+    @Test
+    public void testEvaluateECHigh() {
+        SimulatedMeasurement packetLossMeasurement = SimulatedMeasurement.of(0.06, packetLossSpec);
+        SimulatedMeasurement energyConsumptionMeasurement = SimulatedMeasurement
+            .of(DeltaIoTCommons.UPPER_BOUND_ENERGY_CONSUMPTION + 0.1, energyConsumptionSpec);
+        List<SimulatedMeasurement> measuredQuantities = Arrays.asList(packetLossMeasurement,
+                energyConsumptionMeasurement);
+        StateQuantity quantifiedState = StateQuantity.of(measuredQuantities);
+
+        Reward<Double> actualReward = evaluator.evaluate(quantifiedState);
+
+        assertThat(actualReward.getValue()).isEqualTo(0.7999, withPrecision(EPSILON));
+    }
 }
