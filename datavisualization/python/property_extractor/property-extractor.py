90c7dd057c39b37f68d543f6ae83f5313746f150
Ralf Sieger <sieger@fzi.de>
2026-01-19 09:11:53 +0100

    introduced command line argumenst

--- datavisualization/python/property_extractor/property-extractor.py
+++ datavisualization/python/property_extractor/property-extractor.py
@@ -1,12 +1,13 @@
+import argparse
 from pathlib import Path
 import json
 
-input_dir = Path(r"prism")
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+
 
 def extract_property_name(line):
     # Rmax=? [ F "EnergyConsumption" ]
@@ -14,12 +15,14 @@ def extract_property_name(line):
     name = line.split('"')[1]
     return name
 
+
 def read_property_name(prism_folder, result_number):
     property_file = prism_folder / ("prism_%s.properties" % result_number)
     with open(property_file) as f:
         line = next(f)
         property_name = extract_property_name(line)
         return property_name
+
 
 def read_sample(sample_file):
     with open(sample_file) as f:
@@ -34,14 +37,20 @@ def read_sample(sample_file):
         "property_name": property_name,
     }
 
+
 def main():
-    types = ('*.log') #('*.model', '*.properties', '*.result')  # the tuple of file types
-    files = [f for f in input_dir.iterdir() if f.is_file()]
+    parser = argparse.ArgumentParser(prog="property_extractor", description="Extracts QA properties from PRISM results")
+    default = ' (default: %(default)s)'
+    parser.add_argument('--input-dir', type=Path, help="PRISM folder containing results")
+    parser.add_argument('--sample-count', default=96, help="sample count per run" + default)
+    args = parser.parse_args()
+
+    files = [f for f in args.input_dir.iterdir() if f.is_file()]
     result_files = [file for file in files if file.suffix == ".result"]
     sorted_results = sorted(result_files, key=lambda x: x.stem)
-    sample_count = 96
+
     runs = []
-    for i, samples in enumerate(chunks(sorted_results, sample_count * 2)):
+    for i, samples in enumerate(chunks(sorted_results, args.sample_count * 2)):
         print("run: %d" % (i + 1))
         qas = {}
         for sample_pair in chunks(samples, 2):
@@ -55,6 +64,7 @@ def main():
 
     with open("result.json", "w") as f:
         print(json.dump(runs, f, indent=2))
+
 
 if __name__ == "__main__":
     main()
