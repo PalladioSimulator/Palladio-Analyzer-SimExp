af4d025ce9ea128b798c995096d849a785132c62
Ralf Sieger <sieger@fzi.de>
2026-02-23 10:05:25 +0100

    added pareto front extraction

--- datavisualization/python/simulation_result/simulation_result.py
+++ datavisualization/python/simulation_result/simulation_result.py
@@ -4,6 +4,7 @@ import collections
 import statistics
 import json
 from pathlib import Path
+import re
 
 import tabulate
 
@@ -47,13 +48,13 @@ class SimulationResult:
         print(table_str)
 
 
-    def _read_result_task_file(self, result_file):
-        with result_file.open("r", encoding="utf-8") as f:
+    def _read_json_file(self, json_file):
+        with json_file.open("r", encoding="utf-8") as f:
             result = json.load(f)
             return result
 
     def _analyze_task_result(self, result_file):
-        task_result = self._read_result_task_file(result_file)
+        task_result = self._read_json_file(result_file)
         task_id = task_result["result"]["id"]
         reward = task_result["result"]["reward"]
         runs = task_result["result"]["quality_measurements"]["runs"]
@@ -128,7 +129,7 @@ class SimulationResult:
 
     def _process_task_file(self, writer, task_file):
         print("processing: %s" % task_file.name)
-        task_result = self._read_result_task_file(task_file)
+        task_result = self._read_json_file(task_file)
         task_id = task_result["result"]["id"]
         optimizables = ["%s=%s" % (key, value) for key, value in task_result["optimizables"].items()]
         values = ",".join(optimizables)
@@ -184,6 +185,77 @@ class SimulationResult:
                                       )
         print(table_str)
 
+    def _analyze_pareto(self, args):
+        entries = []
+        for pareto_front_file in args.pareto_front:
+            entry = self._extract_pareto_front(pareto_front_file)
+            entries.append(entry)
+
+        max_gen = max([entry["generation"] for entry in entries])
+        sorted_entries = sorted(entries, key=lambda e: e["generation"] if e["generation"] >= 0 else max_gen+1)
+
+        headers = ['generation', 'file', "entry", "id", "reward", "energy_consumprion_average", "packet_loss_average"]
+        table_entries = []
+        for entry in sorted_entries:
+            for i, front_member in enumerate(entry["members"]):
+                table_entries.append([
+                    entry["generation"], entry["file"],
+                    i, front_member["id"],
+                    front_member["reward"], front_member["energy_consumprion_average"],
+                    front_member["packet_loss_average"],
+                ])
+
+        with args.result.open("w", encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=headers)
+            writer.writeheader()
+            for entry in sorted_entries:
+                for i, front_member in enumerate(entry["members"]):
+                    writer.writerow({'generation': entry["generation"],
+                                     'file': entry["file"],
+                                     'entry': i,
+                                     'id': front_member["id"],
+                                     'reward': front_member["reward"],
+                                     'energy_consumprion_average': front_member["energy_consumprion_average"],
+                                     'packet_loss_average': front_member["packet_loss_average"],
+                                     })
+
+        table_str = tabulate.tabulate(table_entries,
+                                      headers=headers,
+                                      tablefmt="simple"
+                                      )
+        print(table_str)
+
+    def _extract_pareto_front(self, pareto_front_file: Path) -> dict:
+        front = self._read_json_file(pareto_front_file)
+        r = re.compile(r"pareto_front_(\d+)")
+        match = r.match(pareto_front_file.stem)
+        if match:
+            generation = int(match.group(1)) - 1
+        else:
+            generation = -1
+
+        front_members = []
+        for front_entry in front:
+            optimizables = ["%s=%s" % (key, value) for key, value in front_entry["optimizables"].items()]
+            values = ",".join(optimizables)
+
+            front_member = {
+                "id": front_entry["id"],
+                "reward": front_entry["fitness"],
+                "values": values,
+                "energy_consumprion_average": front_entry["averages"]["EnergyConsumption.props"],
+                "packet_loss_average": front_entry["averages"]["PacketLoss.props"],
+            }
+            front_members.append(front_member)
+
+        entry = {
+            "generation": generation,
+            "file": pareto_front_file.stem,
+            "members": front_members,
+        }
+
+        return entry
+
     def main(self):
         parser = argparse.ArgumentParser(prog="simulation_result", description="Analyses simulation results")
         subparsers = parser.add_subparsers(required=True, help='available subcommands')
@@ -205,6 +277,11 @@ class SimulationResult:
         parser_prism = subparsers.add_parser('prism', help='prism result analyzer')
         parser_prism.add_argument('prism_property_file', type=Path, nargs='+')
         parser_prism.set_defaults(func=self._analyze_prism)
+
+        parser_pareto = subparsers.add_parser('pareto', help='pareto front extractor')
+        parser_pareto.add_argument('pareto_front', type=Path, nargs='+', help="pareto front files")
+        parser_pareto.add_argument('-r', '--result', type=Path, required=True, help="result CSV file")
+        parser_pareto.set_defaults(func=self._analyze_pareto)
 
         args = parser.parse_args()
 
