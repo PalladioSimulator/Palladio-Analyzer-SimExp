fde44f35124ce0441512014b0c5806b3d01fa2f4
Ralf Sieger <sieger@fzi.de>
2026-04-22 11:29:16 +0200

    fixed float comparisons

--- datavisualization/python/simulation_result/pareto/reference_point_calculator_test.py
+++ datavisualization/python/simulation_result/pareto/reference_point_calculator_test.py
@@ -1,4 +1,4 @@
-from pytest import fixture
+from pytest import fixture, approx
 
 from .pareto_front import ParetoFront, ParetoEntry
 from .reference_point_calculator import ReferencePointCalculator
@@ -19,7 +19,7 @@ def test_calc_ref_point_one_front_one(calculator):
 
     actual_ref_point = calculator.calc_reference_point(fronts)
 
-    assert actual_ref_point == (1.0, 1.0)
+    assert actual_ref_point == approx((1.0, 1.0))
 
 
 def test_calc_ref_point_one_front_two(calculator):
@@ -33,7 +33,7 @@ def test_calc_ref_point_one_front_two(calculator):
 
     actual_ref_point = calculator.calc_reference_point(fronts)
 
-    assert actual_ref_point == (2.0, 2.0)
+    assert actual_ref_point == approx((2.0, 2.0))
 
 
 def test_calc_ref_point_two_front_one(calculator):
@@ -51,4 +51,4 @@ def test_calc_ref_point_two_front_one(calculator):
 
     actual_ref_point = calculator.calc_reference_point(fronts)
 
-    assert actual_ref_point == (2.0, 2.0)
+    assert actual_ref_point == approx((2.0, 2.0))
