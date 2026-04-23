c7a52b4322f63fc2bf3c6a01b1bbbd18052efff8
Ralf Sieger <sieger@fzi.de>
2026-04-23 16:19:03 +0200

    added test for kubernetes folder

--- datavisualization/python/simulation_result/pareto/util.py
+++ datavisualization/python/simulation_result/pareto/util.py
@@ -8,3 +8,5 @@ def validate_resource_folder(folder: Path):
         raise ValueError("missing final pareto_front.json file in: %s" % folder)
     if not (folder / "generations").is_dir():
         raise ValueError("missing generations folder in: %s" % folder)
+    if not (folder / "kubernetes").is_dir():
+        raise ValueError("missing kubernetes folder in: %s" % folder)
