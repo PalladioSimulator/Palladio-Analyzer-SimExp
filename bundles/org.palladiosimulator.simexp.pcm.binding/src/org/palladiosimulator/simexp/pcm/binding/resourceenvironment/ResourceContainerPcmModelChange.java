1903b64424385b675b55a28645de54dcd4d2cdd9
Nicolas-Boltz <boltz.nicolas@gmx.de>
2026-08-13 10:41:19 +0200

    Migrate from commons-lang 2 to commons-lang3
    org.apache.commons.lang left the release with 2025-12. Five of the six classes in use
    carry the same name in lang3. StringEscapeUtils does not: it was removed from lang3 and
    lives in commons-text, where escapeXml is split into escapeXml10 and escapeXml11 - the
    generated content is XML 1.0, so escapeXml10 takes its place.

--- bundles/org.palladiosimulator.simexp.pcm.binding/src/org/palladiosimulator/simexp/pcm/binding/resourceenvironment/ResourceContainerPcmModelChange.java
+++ bundles/org.palladiosimulator.simexp.pcm.binding/src/org/palladiosimulator/simexp/pcm/binding/resourceenvironment/ResourceContainerPcmModelChange.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.util.EList;
 import org.palladiosimulator.analyzer.workflow.core.blackboard.PCMResourceSetPartition;
--- bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/launcher/SimExpLauncher.java
+++ bundles/org.palladiosimulator.simexp.workflow/src/main/java/org/palladiosimulator/simexp/workflow/launcher/SimExpLauncher.java
@@ -15,7 +15,7 @@ import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
